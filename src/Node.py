4585d73ef2d3118f9a49d6b55f329953424eac07
Alex Kolar <alexkolar@comcast.net>
2019-06-27 12:01:27 -0500

    Updated node attributes and added functionality for OpticalChannel loss

--- src/Node.py
+++ src/Node.py
@@ -4,27 +4,32 @@ from Photon import Photon
 
 class LightSource(Entity):
 
-    def __init__(self, timeline, frequency, wavelength, mean_photon_num, encoding_type, direct_receiver, name=None):
+    def __init__(self, timeline, frequency, wavelength, mean_photon_num, encoding_type, state, name=None):
         Entity.__init__(self, timeline, name)
         self.frequency = frequency
         self.wavelength = wavelength
         self.mean_photon_num = mean_photon_num
         self.encoding_type = encoding_type
-        self.direct_receiver = direct_receiver
+        self.direct_receiver = None
         self.photon_counter = 0
+        self.quantum_state = state
 
     def init(self):
         pass
 
     # emit_photon does not currently account for poisson distribution
-    def emit_photon(self):
-        photon = Photon(self.timeline, self.wavelength, "photon")
+    def emit_photon(self, time):
+        photon = Photon(self.timeline, self.wavelength,
+                        self.direct_receiver, self.encoding_type, self.quantum_state, "photon")
         # TODO: figure out photon naming scheme
 
         self.timeline.entities.append(photon)
-        self.direct_receiver.transmit_photon(photon)
+        self.direct_receiver.transmit(photon)
 
         self.photon_counter += 1
+
+    def assign_receiver(self, receiver):
+        self.direct_receiver = receiver
 
 
 class Detector(Entity):
@@ -39,9 +44,8 @@ class Detector(Entity):
     def init(self):
         pass
 
-    def measure_photon(self, photon):
-        self.timeline.entities.remove(photon)
-        return photon.frequency
+    def detect(self, photon):
+        pass
 
 
 class Memory(Entity):
@@ -58,22 +62,16 @@ class Memory(Entity):
 
 class Node(Entity):
 
-    def __init__(self, timeline, memory=None, source=None, detector=None, connected_channel=None, name=None):
+    def __init__(self, timeline, components=None, name=None):
         Entity.__init__(self, timeline, name)
-        self.memory = memory
-        self.source = source
-        self.detector = detector
-        self.connected_channel = connected_channel
-
-        self.source.direct_receiver = connected_channel
+        self.components = components
 
     def init(self):
         pass
 
-    def send_photon(self):
+    def send_photon(self, time):
         # use emitter to send photon over connected channel to node
-        self.source.emit_photon()
+        self.components["light_source"].emit(time)
 
     def receive_photon(self, photon):
-        # use detector to sense photon (return frequency information)
-        return self.detector.measure_photon(photon)
+        self.components["detector"].detect(photon)
--- src/OpticalChannel.py
+++ src/OpticalChannel.py
@@ -2,28 +2,50 @@ from entity import Entity
 from event import Event
 from process import Process
 from Node import Node
+import numpy as np
 
 
 class OpticalChannel(Entity):
 
-    def __init__(self, timeline, length, attenuation, temperature, fidelity, sender, receiver, name=None):
+    def __init__(self, timeline, attenuation, distance, temperature, fidelity, name=None):
         Entity.__init__(self, timeline, name)
-        self.length = length
         self.attenuation = attenuation
+        self.distance = distance
         self.temperature = temperature
         self.fidelity = fidelity
-        self.sender = sender
-        self.receiver = receiver
+        self.sender = None
+        self.receiver = None
+        self.light_speed = 3 * 10 ** -4 # used for photon timing calculations
 
     def init(self):
         pass
 
-    def transmit_photon(self, photon):
+    def transmit(self, photon):
         # TODO: check if node connected to optical channel
 
-        # future_time will be set based on speed of photon in future
-        future_time = self.timeline.now() + 1
-        process = Process(self.receiver, Node.receive_photon, [photon])
+        # generate chance to lose photon
+        length = self.distance # calculate based on temp
 
-        event = Event(future_time, process)
-        self.timeline.schedule(event)
+        loss = length * self.attenuation
+        chance_photon_kept = 10 ** (loss / -20)
+        chance_photon_kept *= self.fidelity
+
+        # check if photon lost
+        if np.random.random > chance_photon_kept:
+            self.timeline.entities.remove(photon)
+        else:
+            # schedule receiving node to receive photon at future time determined by light speed
+            future_time = self.timeline.now() + int(length/self.light_speed)
+            process = Process(self.receiver, Node.receive_photon, [photon])
+
+            event = Event(future_time, process)
+            self.timeline.schedule(event)
+
+    def set_sender(self, sender):
+        self.sender = sender
+
+    def set_receiver(self, receiver):
+        self.receiver = receiver
+
+    def change_distance(self, distance):
+        self.distance = distance
