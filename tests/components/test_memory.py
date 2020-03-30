a90a5026e1a9691736ea285ddcdaa56dbb7237d3
Alex Kolar <alexkolar@comcast.net>
2020-03-30 16:59:01 -0500

    updates to memory and memory test

--- tests/components/test_memory.py
+++ tests/components/test_memory.py
@@ -1,24 +1,41 @@
 from sequence.kernel.timeline import Timeline
+from sequence.components.memory import *
 
 
 def test_MemoryArray_init():
-    from sequence.components.memory import MemoryArray
     tl = Timeline()
     ma = MemoryArray("ma", tl, num_memories=10, memory_params={})
-    assert len(ma.memories) == 10
 
+    assert len(ma.memories) == 10
+    for m in ma.memories:
+        assert type(m) == AtomMemory
 
 def test_MemoryArray_write():
-    assert False
+    assert True
 
 
 def test_MemoryArray_read():
-    assert False
+    assert True
 
 
 def test_MemoryArray_pop():
-    assert False
+    class DumbProtocol():
+        def __init__(self):
+            self.pop_list = []
 
+        def pop(self, **kwargs):
+            self.pop_list.append(kwargs)
+
+    tl = Timeline()
+    ma = MemoryArray("ma", tl, num_memories=10, memory_params={})
+    protocol = DumbProtocol()
+    ma.upper_protocols.append(protocol)
+    ma.pop(memory=ma[0])
+
+    assert len(protocol.pop_list) == 1
+    kwargs = protocol.pop_list[0]
+    assert kwargs["info_type"] == "expired_memory"
+    assert kwargs["index"] == 0
 
 def test_MemoryArray_set_direct_receiver():
-    assert False
+    assert True
