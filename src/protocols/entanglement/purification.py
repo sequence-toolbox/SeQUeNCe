a16ad995ff1c6b8a4e22817c6d9daaac315ca5b0
Xiaoliang Wu <zurielibra@gmail.com>
2020-04-05 14:06:22 -0500

    update purification.py to fit new framework.

--- src/protocols/entanglement/purification.py
+++ src/protocols/entanglement/purification.py
@@ -1,4 +1,8 @@
-from typing import List
+from typing import List, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from ...components.memory import Memory
+    from ...topology.node import Node
 
 from numpy.random import random
 
@@ -7,231 +11,68 @@ from ..protocol import Protocol
 
 
 class BBPSSWMessage(Message):
-    def __init__(self, msg_type, **kwargs):
+    def __init__(self, msg_type: str, receiver: str, **kwargs):
+        self.receiver = receiver
         self.msg_type = msg_type
-        self.owner_type = type(BBPSSW(None, 0))
-        if self.msg_type == "PING":
-            self.index = kwargs["index"]
-            self.kept_memo_r = kwargs["kept_memo_r"]
-            self.meas_memo_r = kwargs["meas_memo_r"]
-            self.kept_memo_s = kwargs["kept_memo_s"]
-            self.meas_memo_s = kwargs["meas_memo_s"]
-        elif self.msg_type == "PONG":
-            self.index = kwargs["index"]
-            self.fidelity = kwargs["fidelity"]
-            self.kept_memo_r = kwargs["kept_memo_r"]
-            self.meas_memo_r = kwargs["meas_memo_r"]
+        if self.msg_type == "PURIFICATION_RES":
+            pass
         else:
             raise Exception("BBPSSW protocol create unkown type of message: %s" % str(msg_type))
 
-    def __str__(self):
-        if self.msg_type == "PING":
-            return "BBPSSW Message: msg_type: %s; round #: %d; kept memo id (receiver): %d; " \
-                   "measured memo id (receiver): %d; kept memo id (sender): %d; measured memo id (sender): %d;" \
-                   % (self.msg_type, self.index, self.kept_memo_r, self.meas_memo_r, self.kept_memo_s, self.meas_memo_s)
-        elif self.msg_type == "PONG":
-            return "BBPSSW Message: msg_type: %s; round #: %d; kept memo id (receiver): %d; " \
-                   "measured memo id (receiver): %d; fidelity: %.2f" \
-                   % (self.msg_type, self.index, self.kept_memo_r, self.meas_memo_r, self.fidelity)
-
 
 class BBPSSW(Protocol):
-    '''
-    BBPSSW use PING, PONG message to exchange classical information
-    PING message is composed by five parts:
-        1. Type of message: PING
-        2. The index number of operated purification: integer
-        3. Memory id of kept memory on message receiver: integer
-        4. Memory id of measured memory on message receiver: integer
-        5. Memory id of kept memory on message sender: integer
-        6. Memory id of measured memory on message sender: integer
-    PONG message is composed by four parts:
-        1. Type of message: PONG
-        2. The index number of operated purification: integer
-        3. Fidelity after purification: float
-        4. Memory id of kept memory on message receiver: integer
-        5. Memory id of measured memory on message receiver: integer
-    ASSUMPTION:
-        1. Two nodes receive poped message from bottom layer before receive
-           PING / PONG message
-        2. Classical message
-        3. nodes have different name
-    '''
-
-    def __init__(self, own, threshold):
-        if own is None:
-            # to create dummy object with none parameters
-            return
+    def __init__(self, own: "Node", name: str, kept_memo: "Memory", meas_memo: "Memory"):
+        assert kept_memo != meas_memo
         Protocol.__init__(self, own)
-        self.threshold = threshold
-        # self.purified_lists :
-        # { node name : [ [index of memories after round i purificaiton] ]
-        self.purified_lists = {}
-        # self.waiting_list:
-        # { round of purification : [ set( [ kept memory, measured memory ] ) }
-        self.waiting_list = {}
-        self.rsvp_name = ''
-        self.valid_memories = set()
+        self.name = name
+        self.kept_memo = kept_memo
+        self.meas_memo = meas_memo
+        self.another = None
+        self.is_success = None
 
-    def init(self):
-        pass
+    def set_another(self, another: "BBPSSW"):
+        self.another = another
 
-    def set_valid_memories(self, memories):
-        self.valid_memories = memories
+    def start(self) -> None:
+        assert self.another is not None, "another protocol is not setted; please use set_another function to set it."
+        assert (self.kept_memo.entangled_memory["node_id"] ==
+                self.meas_memo.entangled_memory["node_id"] ==
+                self.another.own.name)
+        assert self.kept_memo.fidelity == self.meas_memo.fidelity > 0.5
 
-    def pop(self, **kwargs):
-        if "info_type" in kwargs:
-            return
-        memory_index = kwargs["memory_index"]
-        another_node = kwargs["another_node"]
-        if memory_index not in self.valid_memories:
-            return False
+        if self.is_success is None:
+            if random() > self.success_probability(self.kept_memo.fidelity):
+                self.is_success = self.another.is_success = True
+            else:
+                self.is_success = self.another.is_success = False
 
-        if another_node not in self.purified_lists:
-            self.purified_lists[another_node] = []
-        purified_list = self.purified_lists[another_node]
-        if len(purified_list) == 0:
-            purified_list.append([])
-
-        local_memory = self.own.components['MemoryArray']
-        cur_fidelity = local_memory[memory_index].fidelity
-
-        if cur_fidelity < self.threshold:
-            purified_list[0].append(memory_index)
+        if self.is_success:
+            self.kept_memo.fidelity = self.improved_fidelity(self.kept_memo.fidelity)
+            dst = self.another.own.name
+            message = BBPSSWMessage("PURIFICATION_RES", self.another.name)
+            self.own.send_message(dst, message)
         else:
-            self._pop(memory_index=memory_index, another_node=another_node)
+            self.kept_memo.fidelity = 0
+            self.kept_memo.entangled_memory["node_id"] = None
+            self.kept_memo.entangled_memory["memo_id"] = None
+            dst = self.another.own.name
+            message = BBPSSWMessage("PURIFICATION_RES", self.another.name)
+            self.own.send_message(dst, message)
 
-        if len(purified_list[0]) > 1 and self.own.name > another_node:
-            self.start_round(0, another_node)
+        self.meas_memo.fidelity = 0
+        self.meas_memo.entangled_memory["node_id"] = None
+        self.meas_memo.entangled_memory["memo_id"] = None
+        self.update_resource_manager(self.meas_memo, "EMPTY")
 
-        return True
+    def update_resource_manager(self, memory: "Memory", state: str):
+        self.own.resource_manager.update(memory, state)
 
-    def start_round(self, round_id, another_node):
-        local_memory = self.own.components['MemoryArray']
-        purified_list = self.purified_lists[another_node]
-        if round_id not in self.waiting_list:
-            self.waiting_list[round_id] = set()
-        kept_memo = purified_list[round_id].pop()
-        measured_memo = purified_list[round_id].pop()
-        assert (local_memory[kept_memo].fidelity == local_memory[measured_memo].fidelity)
-        assert (local_memory[kept_memo].fidelity > 0.5)
-
-        another_kept_memo = local_memory[kept_memo].entangled_memory['memo_id']
-        another_measured_memo = local_memory[measured_memo].entangled_memory['memo_id']
-        self.waiting_list[round_id].add((kept_memo, measured_memo))
-
-        msg = BBPSSWMessage("PING", index=round_id, kept_memo_r=another_kept_memo,
-                            meas_memo_r=another_measured_memo, kept_memo_s=kept_memo,
-                            meas_memo_s=measured_memo)
-
-        # WARN: wait change of Node.send_message function
-        self.own.send_message(dst=another_node, msg=msg, priority=2)
-
-    def push(self, **kwargs):
-        self._push(**kwargs)
-
-    def received_message(self, src: str, msg: Message):
-        if src not in self.purified_lists:
-            return False
-        purified_list = self.purified_lists[src]
-        # WARN: wait change of Node.receive_message
-        if msg.msg_type == "PING":
-            round_id = msg.index
-            kept_memo = msg.kept_memo_r
-            measured_memo = msg.meas_memo_r
-            if not (len(purified_list) > round_id and
-                    kept_memo in purified_list[round_id] and
-                    measured_memo in purified_list[round_id]):
-                return False
-            fidelity = self.purification(round_id, kept_memo,
-                                         measured_memo, purified_list)
-
-            reply = BBPSSWMessage("PONG", index=round_id, fidelity=fidelity,
-                                  kept_memo_r=msg.kept_memo_s, meas_memo_r=msg.meas_memo_s)
-
-            # WARN: wait change of Node.send_message function
-            self.own.send_message(dst=src, msg=reply, priority=2)
-
-            if fidelity >= self.threshold:
-                self._pop(memory_index=kept_memo, another_node=src)
-                purified_list[round_id + 1].remove(kept_memo)
-        elif msg.msg_type == "PONG":
-            round_id = msg.index
-            fidelity = msg.fidelity
-            kept_memo = msg.kept_memo_r
-            measured_memo = msg.meas_memo_r
-            if round_id not in self.waiting_list or (kept_memo, measured_memo) not in self.waiting_list[round_id]:
-                return False
-
-            self.update(round_id, fidelity, kept_memo,
-                        measured_memo, purified_list)
-            if fidelity >= self.threshold:
-                self._pop(memory_index=kept_memo, another_node=src)
-            if (round_id + 1 < len(purified_list) and len(purified_list[round_id + 1]) > 1):
-                self.start_round(round_id + 1, src)
+    def received_message(self, src: str, msg: List[str]):
+        assert src == self.another.own.name
+        if self.is_success is True:
+            self.update_resource_manager(self.kept_memo, state="ENTANGLE")
         else:
-            raise Exception("BBPSSW protocol receives"
-                            "unkown type of message: %s" % str(msg))
-
-        return True
-
-    def purification(self,
-                     round_id: int,
-                     kept_memo: int,
-                     measured_memo: int,
-                     purified_list: List[List[int]]) -> float:
-
-        local_memory = self.own.components['MemoryArray']
-
-        assert (local_memory[kept_memo].fidelity ==
-                local_memory[measured_memo].fidelity)
-        assert (local_memory[kept_memo].fidelity > 0.5)
-        purified_list[round_id].remove(kept_memo)
-        purified_list[round_id].remove(measured_memo)
-
-        fidelity = local_memory[kept_memo].fidelity
-        suc_prob = self.success_probability(fidelity)
-        if random() < suc_prob:
-            fidelity = round(self.improved_fidelity(fidelity), 6)
-            local_memory[kept_memo].fidelity = fidelity
-
-            if len(purified_list) <= round_id + 1:
-                purified_list.append([])
-            purified_list[round_id + 1].append(kept_memo)
-        else:
-            fidelity = 0
-            local_memory[kept_memo].fidelity = fidelity
-            local_memory[kept_memo].entangled_memory['node_id'] = None
-            local_memory[kept_memo].entangled_memory['memo_id'] = None
-            self._push(index=kept_memo)
-
-        local_memory[measured_memo].fidelity = 0
-        local_memory[measured_memo].entangled_memory['node_id'] = None
-        local_memory[measured_memo].entangled_memory['memo_id'] = None
-        self._push(index=measured_memo)
-        return fidelity
-
-    def update(self, round_id: int,
-               fidelity: float, kept_memo: int,
-               measured_memo: int, purified_list):
-
-        local_memory = self.own.components['MemoryArray']
-        self.waiting_list[round_id].remove((kept_memo, measured_memo))
-
-        local_memory[kept_memo].fidelity = fidelity
-        if fidelity == 0:
-            local_memory[kept_memo].entangled_memory['node_id'] = None
-            local_memory[kept_memo].entangled_memory['memo_id'] = None
-            self._push(index=kept_memo)
-        elif fidelity < self.threshold:
-            if len(purified_list) <= round_id + 1:
-                purified_list.append([])
-            purified_list[round_id + 1].append(kept_memo)
-
-        local_memory[measured_memo].fidelity = 0
-        local_memory[measured_memo].entangled_memory['node_id'] = None
-        local_memory[measured_memo].entangled_memory['memo_id'] = None
-        self._push(index=measured_memo)
+            self.update_resource_manager(self.kept_memo, state="EMPTY")
 
     @staticmethod
     def success_probability(F: float) -> float:
@@ -239,7 +80,7 @@ class BBPSSW(Protocol):
         F is the fidelity of entanglement
         Formula comes from Dur and Briegel (2007) page 14
         '''
-        return F**2 + 2 * F * (1 - F) / 3 + 5 * ((1 - F) / 3)**2
+        return F ** 2 + 2 * F * (1 - F) / 3 + 5 * ((1 - F) / 3) ** 2
 
     @staticmethod
     def improved_fidelity(F: float) -> float:
@@ -247,5 +88,10 @@ class BBPSSW(Protocol):
         F is the fidelity of entanglement
         Formula comes from Dur and Briegel (2007) formula (18) page 14
         '''
-        return (F**2 + ((1 - F) / 3)**2) / (F**2 + 2 * F * (1 - F) / 3 + 5 * ((1 - F) / 3)**2)
+        return (F ** 2 + ((1 - F) / 3) ** 2) / (F ** 2 + 2 * F * (1 - F) / 3 + 5 * ((1 - F) / 3) ** 2)
 
+    def push(self, **kwargs):
+        pass
+
+    def pop(self, **kwargs):
+        pass
--- tests/protocols/entanglement/test_purification.py
+++ tests/protocols/entanglement/test_purification.py
@@ -1,5 +1,6 @@
 import numpy
-from sequence.components.memory import MemoryArray
+import pytest
+from sequence.components.memory import AtomMemory
 from sequence.components.optical_channel import ClassicalChannel
 from sequence.kernel.timeline import Timeline
 from sequence.protocols.entanglement.purification import *
@@ -8,173 +9,81 @@ from sequence.topology.node import Node
 numpy.random.seed(0)
 
 
+class ResourceManager():
+    def __init__(self):
+        self.log = []
+
+    def update(self, memory, state):
+        self.log.append((memory, state))
+
+
 class FakeNode(Node):
     def __init__(self, name, tl, **kwargs):
         Node.__init__(self, name, tl)
-        self.msg_log = None
+        self.msg_log = []
+        self.resource_manager = ResourceManager()
 
     def receive_message(self, src: str, msg: Message):
-        self.msg_log = [self.timeline.now(), src, msg]
+        self.msg_log.append((self.timeline.now(), src, msg))
+        for protocol in self.protocols:
+            if protocol.name == msg.receiver:
+                protocol.received_message(src, msg)
 
 
 def test_BBPSSWMessage():
-    # PING message
-    msg = BBPSSWMessage("PING", index=1, kept_memo_r=0, meas_memo_r=1, kept_memo_s=10, meas_memo_s=11)
-    assert str(msg) == "BBPSSW Message: msg_type: PING; round #: 1; kept memo id (receiver): 0; " \
-                       "measured memo id (receiver): 1; kept memo id (sender): 10; measured memo id (sender): 11;"
-    # PONG message
-    msg = BBPSSWMessage("PONG", index=1, fidelity=0.9, kept_memo_r=0, meas_memo_r=1)
-    assert str(msg) == "BBPSSW Message: msg_type: PONG; round #: 1; kept memo id (receiver): 0; " \
-                       "measured memo id (receiver): 1; fidelity: 0.90"
+    msg = BBPSSWMessage("PURIFICATION_RES", "another")
+    assert msg.msg_type == "PURIFICATION_RES" and msg.receiver == "another"
+    with pytest.raises(Exception):
+        BBPSSWMessage("unknown type")
 
 
-def test_BBPSSW_init():
+def test_BBPSSW():
     tl = Timeline()
-    a1 = Node("a1", tl)
-    ep = BBPSSW(a1, 0.9)
-    assert ep in a1.protocols
-    assert ep.threshold == 0.9
-    ep.init()
-
-
-def test_BBPSSW_set_valid_memories():
-    tl = Timeline()
-    a1 = Node("a1", tl)
-    ep = BBPSSW(a1, 0.9)
-    ep.set_valid_memories(set(range(10)))
-    assert ep.valid_memories == set(range(10))
-
-
-def test_BBPSSW_pop():
-    class Parent():
-        def __init__(self):
-            self.last_pop = []
-
-        def pop(self, **kwargs):
-            self.last_pop = [kwargs["memory_index"], kwargs["another_node"]]
-
-    tl = Timeline()
-    a1 = Node("a1", tl)
-    ep = BBPSSW(a1, 0.9)
-    ep.set_valid_memories(set(range(10)))
-
-    # out of scope memory
-    assert ep.pop(memory_index=100, another_node="a0") is False
-
-    # qualified memory
-    parent = Parent()
-    ep.upper_protocols.append(parent)
-    ma = MemoryArray("ma", tl, num_memories=10, memory_params={})
-    a1.assign_component(ma, "MemoryArray")
-    ma[0].fidelity = 0.99
-    ep.pop(memory_index=0, another_node="a0")
-    assert parent.last_pop == [0, "a0"]
-
-    # unqualified one memory
-    ma[1].fidelity = 0.8
-    assert ep.pop(memory_index=1, another_node="a0") is True
-    assert ep.purified_lists["a0"][0] == [1]
-
-    ma[2].fidelity = 0.8
-    assert ep.pop(memory_index=2, another_node="a2") is True
-    assert ep.purified_lists["a0"][0] == [1] and ep.purified_lists["a2"][0] == [2]
-
-
-def test_BBPSSW_start_round():
-    tl = Timeline()
-    a1 = Node("a1", tl)
-    ep = BBPSSW(a1, 0.9)
-    ep.set_valid_memories(set(range(10)))
-    ep.purified_lists["a2"] = [[]]
-    ep.purified_lists["a2"][0] = [0, 1]
-
-    ma = MemoryArray("ma", tl, num_memories=10, memory_params={})
-    a1.assign_component(ma, "MemoryArray")
-    ma[0].fidelity = ma[1].fidelity = 0.8
-    ma[0].entangled_memory = {"memo_id": 10}
-    ma[1].entangled_memory = {"memo_id": 11}
-
+    a1 = FakeNode("a1", tl)
     a2 = FakeNode("a2", tl)
-    cc = ClassicalChannel("cc", tl, 2e-4, 1e3)
+    cc = ClassicalChannel("cc", tl, 0, 1e5)
+    cc.delay = 1e9
     cc.set_ends(a1, a2)
 
-    ep.start_round(0, "a2")
-    res = set()
-    res.add((1, 0))
-    assert ep.waiting_list[0] == res
-    tl.run()
-    assert a2.msg_log[0] == cc.delay and a2.msg_log[1] == a1.name
-    assert a2.msg_log[2].index == 0 and a2.msg_log[2].kept_memo_r == 11 and a2.msg_log[2].meas_memo_r == 10 \
-           and a2.msg_log[2].kept_memo_s == 1 and a2.msg_log[2].meas_memo_s == 0
+    tl.init()
+    for i in range(1000):
+        fidelity = numpy.random.uniform(0.5, 1)
+        kept_memo1 = AtomMemory("a1.kept", tl, fidelity=fidelity)
+        kept_memo2 = AtomMemory("a2.kept", tl, fidelity=fidelity)
+        meas_memo1 = AtomMemory("a1.meas", tl, fidelity=fidelity)
+        meas_memo2 = AtomMemory("a2.meas", tl, fidelity=fidelity)
 
+        kept_memo1.entangled_memory["node_id"] = "a2"
+        kept_memo1.entangled_memory["memo_id"] = "a2.kept"
+        kept_memo2.entangled_memory["node_id"] = "a1"
+        kept_memo2.entangled_memory["memo_id"] = "a1.kept"
+        meas_memo1.entangled_memory["node_id"] = "a2"
+        meas_memo1.entangled_memory["memo_id"] = "a2.meas"
+        meas_memo2.entangled_memory["node_id"] = "a1"
+        meas_memo2.entangled_memory["memo_id"] = "a1.meas"
 
-def test_BBPSSW_received_message():
-    tl = Timeline()
-    a1 = Node("a1", tl)
-    ep = BBPSSW(a1, 0.9)
-    ep.set_valid_memories(set(range(10)))
-    ep.purified_lists["a2"] = [[0, 1]]
+        ep1 = BBPSSW(a1, "a1.ep1.%d" % i, kept_memo1, meas_memo1)
+        ep2 = BBPSSW(a2, "a2.ep2.%d" % i, kept_memo2, meas_memo2)
+        ep1.set_another(ep2)
+        ep2.set_another(ep1)
 
-    ma = MemoryArray("ma", tl, num_memories=10, memory_params={})
-    a1.assign_component(ma, "MemoryArray")
-    ma[0].fidelity = ma[1].fidelity = 0.8
-    ma[0].entangled_memory = {"memo_id": 10}
-    ma[1].entangled_memory = {"memo_id": 11}
+        ep1.start()
+        ep2.start()
 
-    a2 = FakeNode("a2", tl)
-    cc = ClassicalChannel("cc", tl, 2e-4, 1e3)
-    cc.set_ends(a1, a2)
+        assert ep1.is_success == ep2.is_success
+        assert a1.resource_manager.log[-1] == (meas_memo1, "EMPTY")
+        assert a2.resource_manager.log[-1] == (meas_memo2, "EMPTY")
+        assert meas_memo1.fidelity == meas_memo2.fidelity == 0
 
-    # message from unknown node
-    assert ep.received_message("unknown", BBPSSWMessage("PING", index=None, kept_memo_r=None,
-                                                        meas_memo_r=None, kept_memo_s=None, meas_memo_s=None)) is False
+        tl.run()
 
-    # PING message
-    msg = BBPSSWMessage("PING", index=0, kept_memo_r=0, meas_memo_r=1, kept_memo_s=10, meas_memo_s=11)
-    ep.received_message("a2", msg)
-    assert 0 not in ep.purified_lists["a2"][0] and 1 not in ep.purified_lists["a2"][0]
-    assert 0 in ep.purified_lists["a2"][1]
-    tl.run()
-    assert a2.msg_log[0] == cc.delay and a2.msg_log[1] == a1.name
-    assert a2.msg_log[2].index == 0 and a2.msg_log[2].fidelity - 0.84 < 0.01 \
-           and a2.msg_log[2].kept_memo_r == 10 and a2.msg_log[2].meas_memo_r == 11
-
-    # PONG message
-    tl = Timeline()
-    a1 = Node("a1", tl)
-    ep = BBPSSW(a1, 0.9)
-    ep.set_valid_memories(set(range(10)))
-    ep.purified_lists["a2"] = [[]]
-    ep.waiting_list[0] = set()
-    ep.waiting_list[0].add((0, 1))
-    ep.waiting_list[0].add((2, 3))
-
-    ma = MemoryArray("ma", tl, num_memories=10, memory_params={})
-    a1.assign_component(ma, "MemoryArray")
-    ma[0].fidelity = ma[1].fidelity = 0.8
-    ma[0].entangled_memory = {"memo_id": 10}
-    ma[1].entangled_memory = {"memo_id": 11}
-
-    a2 = FakeNode("a2", tl)
-    cc = ClassicalChannel("cc", tl, 2e-4, 1e3)
-    cc.set_ends(a1, a2)
-
-    msg = BBPSSWMessage("PONG", index=0, fidelity=0.88, kept_memo_r=0, meas_memo_r=1)
-    ep.received_message("a2", msg)
-    assert ma[0].fidelity == 0.88 and 0 in ep.purified_lists["a2"][1]
-
-
-def test_BBPSSW_purification():
-    assert False
-
-
-def test_BBPSSW_update():
-    assert False
-
-
-def test_BBPSSW_success_probability():
-    assert False
-
-
-def test_improved_fidelity():
-    assert False
+        if ep1.is_success:
+            assert kept_memo1.fidelity == kept_memo2.fidelity == BBPSSW.improved_fidelity(fidelity)
+            assert kept_memo1.entangled_memory["node_id"] == "a2" and kept_memo2.entangled_memory["node_id"] == "a1"
+            assert a1.resource_manager.log[-1] == (kept_memo1, "ENTANGLE")
+            assert a2.resource_manager.log[-1] == (kept_memo2, "ENTANGLE")
+        else:
+            assert kept_memo1.fidelity == kept_memo2.fidelity == 0
+            assert kept_memo1.entangled_memory["node_id"] == kept_memo2.entangled_memory["node_id"] == None
+            assert a1.resource_manager.log[-1] == (kept_memo1, "EMPTY")
+            assert a2.resource_manager.log[-1] == (kept_memo2, "EMPTY")
