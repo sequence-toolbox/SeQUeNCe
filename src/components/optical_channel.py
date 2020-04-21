7b90c094c2763980550b24bb138cc86e1fd0332d
Xiaoliang Wu <zurielibra@gmail.com>
2020-04-20 20:59:10 -0500

    label arguments type for optical_channel.py

--- src/components/optical_channel.py
+++ src/components/optical_channel.py
@@ -1,6 +1,13 @@
-import numpy
 import heapq as hq
-import warnings
+from typing import TYPE_CHECKING
+
+import numpy
+
+if TYPE_CHECKING:
+    from ..kernel.timeline import Timeline
+    from ..topology.node import Node
+    from ..components.photon import Photon
+    from ..protocols.message import Message
 
 from ..kernel.entity import Entity
 from ..kernel.event import Event
@@ -8,7 +15,7 @@ from ..kernel.process import Process
 
 
 class OpticalChannel(Entity):
-    def __init__(self, name, timeline, attenuation, distance, **kwargs):
+    def __init__(self, name: str, timeline: "Timeline", attenuation: float, distance: int, **kwargs):
         Entity.__init__(self, name, timeline)
         self.attenuation = attenuation
         self.distance = distance  # (measured in m)
@@ -17,33 +24,33 @@ class OpticalChannel(Entity):
                                       2 * 10 ** -4)  # used for photon timing calculations (measured in m/ps)
         # self.chromatic_dispersion = kwargs.get("cd", 17)  # measured in ps / (nm * km)
 
-    def init(self):
+    def init(self) -> None:
         pass
 
-    def set_distance(self, distance):
+    def set_distance(self, distance: int) -> None:
         self.distance = distance
 
 
 class QuantumChannel(OpticalChannel):
-    def __init__(self, name, timeline, attenuation, distance, **kwargs):
+    def __init__(self, name: str, timeline: "Timeline", attenuation: float, distance: int, **kwargs):
         super().__init__(name, timeline, attenuation, distance, **kwargs)
         self.ends = []
         self.delay = 0
         self.loss = 1
-        self.frequency = kwargs.get("frequency", 1e12) # frequency at which send qubits (measured in Hz)
+        self.frequency = kwargs.get("frequency", 1e12)  # frequency at which send qubits (measured in Hz)
         self.send_bins = []
 
-    def init(self):
+    def init(self) -> None:
         self.delay = round(self.distance / self.light_speed)
         self.loss = 1 - 10 ** (self.distance * self.attenuation / -10)
 
-    def set_ends(self, end1, end2):
+    def set_ends(self, end1: "Node", end2: "Node") -> None:
         self.ends.append(end1)
         self.ends.append(end2)
         end1.assign_qchannel(self, end2.name)
         end2.assign_qchannel(self, end1.name)
 
-    def transmit(self, qubit, source):
+    def transmit(self, qubit: "Photon", source: "Node") -> None:
         assert self.delay != 0 and self.loss != 1, "QuantumChannel forgets to run init() function"
 
         # remove lowest time bin
@@ -74,10 +81,10 @@ class QuantumChannel(OpticalChannel):
         else:
             pass
 
-    def schedule_transmit(self, min_time):
+    def schedule_transmit(self, min_time: int) -> int:
         min_time = max(min_time, self.timeline.now())
         time_bin = int((min_time * self.frequency) / 1e12) + 1
-       
+
         # find earliest available time bin
         while time_bin in self.send_bins:
             time_bin += 1
@@ -87,62 +94,20 @@ class QuantumChannel(OpticalChannel):
         time = int((time_bin * 1e12) / self.frequency)
         return time
 
-# class QuantumChannel(OpticalChannel):
-#     def __init__(self, name, timeline, attenuation, distance, **kwargs):
-#         super().__init__(name, timeline, attenuation, distance, **kwargs)
-#         self.sender = None
-#         self.receiver = None
-#         self.depo_counter = 0
-#         self.photon_counter = 0
-#         self.delay = 0
-#         self.loss = 1
-#
-#     def init(self):
-#         self.delay = round(self.distance / self.light_speed)
-#         self.loss = 1 - 10 ** (self.distance * self.attenuation / -10)
-#
-#     def set_sender(self, sender):
-#         self.sender = sender
-#
-#     def set_receiver(self, receiver):
-#         self.receiver = receiver
-#
-#     def get(self, photon):
-#         assert self.delay != 0 and self.loss != 1, "QuantumChannel forgets to run init() function"
-#         # check if photon kept
-#         if photon.is_null or numpy.random.random_sample() > self.loss:
-#             self.photon_counter += 1
-#
-#             # check if random polarization noise applied
-#             if numpy.random.random_sample() > self.polarization_fidelity and \
-#                     photon.encoding_type["name"] == "polarization":
-#                 photon.random_noise()
-#                 self.depo_counter += 1
-#
-#             # schedule receiving node to receive photon at future time determined by light speed and dispersion
-#             future_time = self.timeline.now() + self.delay
-#             # dispersion_time = int(round(self.chromatic_dispersion * photon.wavelength * self.distance * 1e-3))
-#
-#             process = Process(self.receiver, "get", [photon])
-#             event = Event(future_time, process)
-#             self.timeline.schedule(event)
-#         else:
-#             pass
-
 
 class ClassicalChannel(OpticalChannel):
-    def __init__(self, name, timeline, attenuation, distance, **kwargs):
+    def __init__(self, name: str, timeline: "Timeline", attenuation: float, distance: int, **kwargs):
         super().__init__(name, timeline, attenuation, distance, **kwargs)
         self.ends = []
         self.delay = kwargs.get("delay", (self.distance / self.light_speed))
 
-    def set_ends(self, end1, end2):
+    def set_ends(self, end1: "Node", end2: "Node") -> None:
         self.ends.append(end1)
         self.ends.append(end2)
         end1.assign_cchannel(self, end2.name)
         end2.assign_cchannel(self, end1.name)
 
-    def transmit(self, message, source, priority: int):
+    def transmit(self, message: "Message", source: "Node", priority: int) -> None:
         # get node that's not equal to source
         if source not in self.ends:
             raise Exception("no endpoint", source)
