ca60ce90695dbb1e4cd1b479a327c67e7b433e90
Alex Kolar <alexkolar@comcast.net>
2020-06-08 18:14:26 -0500

    updates to distance_timebin example

--- example/distance_timebin.py
+++ example/distance_timebin.py
@@ -2,72 +2,98 @@ from numpy import random
 import math
 import statistics
 
-from event import Event
-from timeline import Timeline
-from BB84 import BB84
-import topology
-from process import Process
-import encoding
+import sequence
+from sequence.kernel.event import Event
+from sequence.kernel.process import Process
+from sequence.kernel.timeline import Timeline
+from sequence.protocols.protocol import StackProtocol
+from sequence.protocols.qkd.BB84 import *
+from sequence.components.optical_channel import *
+from sequence.topology.node import *
+from sequence.utils.encoding import *
+
+
+# dummy parent class to receive BB84 keys and initiate BB84
+class Parent(StackProtocol):
+    def __init__(self, own: "Node", keysize: int, role: str):
+        super().__init__(own, "")
+        self.upper_protocols = []
+        self.lower_protocols = []
+        self.keysize = keysize
+        self.role = role
+
+    def init(self):
+        pass
+
+    def pop(self, msg):
+        pass
+
+    def push(self):
+        self.lower_protocols[0].push(self.keysize, 10)
+
+    def received_message(self):
+        pass
 
 
 if __name__ == "__main__":
     random.seed(1)
 
     runtime = 1e12
+    keysize = 256
     distances = [1, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]  # distances in km
     errors = []  # store error rates
     throughputs = []  # store throughputs
 
+
     for distance in distances:
         tl = Timeline(runtime)
-        qc = topology.QuantumChannel("qc", tl,
-                                     distance=distance * 1e3, polarization_fidelity=0.97, attenuation=0.0002)
-        cc = topology.ClassicalChannel("cc", tl,
-                                       distance=distance * 1e3)
+        qc = QuantumChannel("qc", tl, distance=distance * 1e3, polarization_fidelity=0.97, attenuation=0.0002)
+        cc = ClassicalChannel("cc", tl, distance=distance * 1e3)
 
         # Alice
-        ls = topology.LightSource("alice.lightsource", tl,
-                                  encoding_type=encoding.time_bin, frequency=80e6, mean_photon_num=0.1,
-                                  direct_receiver=qc, linewidth=0.5)
-        components = {"lightsource": ls, "cchannel": cc, "qchannel": qc}
-        alice = topology.Node("alice", tl, components=components)
-        qc.set_sender(ls)
-        cc.add_end(alice)
-        tl.entities.append(alice)
+        ls_params = {"frequency": 80e6, "mean_photon_num": 0.1}
+        alice = QKDNode("alice", tl, encoding=time_bin, stack_size=1)
+
+        for name, param in ls_params.items():
+            alice.update_lightsource_params(name, param)
 
         # Bob
-        detectors = [{"efficiency": 0.8, "dark_count": 1, "time_resolution": 10},
-                     {"efficiency": 0.8, "dark_count": 1, "time_resolution": 10},
-                     {"efficiency": 0.8, "dark_count": 1, "time_resolution": 10}]
-        interferometer = {"path_difference": encoding.time_bin["bin_separation"]}
-        switch = {}
-        qsd = topology.QSDetector("bob.qsdetector", tl,
-                                  encoding_type=encoding.time_bin, detectors=detectors, interferometer=interferometer,
-                                  switch=switch)
-        components = {"detector": qsd, "cchannel": cc, "qchannel": qc}
-        bob = topology.Node("bob", tl, components=components)
-        qc.set_receiver(qsd)
-        cc.add_end(bob)
-        tl.entities.append(bob)
+        detector_params = [{"efficiency": 0.8, "dark_count": 1, "time_resolution": 10},
+                           {"efficiency": 0.8, "dark_count": 1, "time_resolution": 10},
+                           {"efficiency": 0.8, "dark_count": 1, "time_resolution": 10}] 
+        bob = QKDNode("bob", tl, encoding=time_bin, stack_size=1)
 
-        # BB84
-        bba = BB84("bba", tl, role=0, encoding_type=0)
-        bbb = BB84("bbb", tl, role=1, encoding_type=0)
-        bba.assign_node(alice)
-        bbb.assign_node(bob)
-        bba.another = bbb
-        bbb.another = bba
-        alice.protocol = bba
-        bob.protocol = bbb
+        for i in range(len(detector_params)):
+            for name, param in detector_params[i].items():
+                bob.update_detector_params(i, name, param)
 
-        process = Process(bba, "generate_key", [256, 10, runtime])
+        qc.set_ends(alice, bob)
+        cc.set_ends(alice, bob)
+
+        # BB84 config
+        pair_bb84_protocols(alice.protocol_stack[0], bob.protocol_stack[0])
+
+        # add parent protocol for monitoring
+        pa = Parent(alice, keysize, "alice")
+        pb = Parent(bob, keysize, "bob")
+        alice.protocol_stack[0].upper_protocols.append(pa)
+        pa.lower_protocols.append(alice.protocol_stack[0])
+        bob.protocol_stack[0].upper_protocols.append(pb)
+        pb.lower_protocols.append(bob.protocol_stack[0])
+
+        process = Process(pa, "push", [])
         event = Event(0, process)
         tl.schedule(event)
 
+        tl.init()
         tl.run()
+        
+        errors.append(statistics.mean(alice.protocol_stack[0].error_rates))
+        throughputs.append(statistics.mean(alice.protocol_stack[0].throughputs))
+        
+        print("completed distance {}".format(distance))
 
-        errors.append(statistics.mean(bba.error_rates))
-        throughputs.append(statistics.mean(bba.throughputs) * 1e-6)
 
-    print(errors)
-    print(throughputs)
+    print("distances: {}".format(distances))
+    print("error rates: {}".format(errors))
+    print("throughputs (bit/s): {}".format(throughputs))
