f6c52999c92ef3cee85aa6299ca8fd451f660d7f
Alex Kolar <alexkolar@comcast.net>
2020-07-01 17:07:47 -0500

    update readme, small changes to makefile and setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.1",
     author="Xiaoliang Wu, Joaquin Chung, Alexander Kolar, Eugene Wang, Tian Zhong, Rajkumar Kettimuthu, Martin Suchara",
     author_email="xwu64@hawk.iit.edu, chungmiranda@anl.gov, akolar@anl.gov, eugenewang@yahoo.com, tzh@uchicago.edu, kettimut@mcs.anl.gov, msuchara@anl.gov",
-    description="Simulator of Quantum Network Communication: SEQUENCE-Python is a prototype version of official SEQUENCE release. ",
+    description="Simulator of Quantum Network Communication: SEQUENCE-Python is a prototype version of the official SEQUENCE release.",
     # packages = find_packages('src'),
     packages=['sequence', 'sequence.app', 'sequence.kernel', 'sequence.components', 'sequence.protocols',
               'sequence.protocols.network_management',
@@ -13,9 +13,8 @@ setup(
               'sequence.topology', 'sequence.utils'],
     package_dir={'sequence': 'src'},
     install_requires=[
-        'setuptools',
+        'numpy',
         'json5',
-        'numba',
         'pandas'
     ],
 )
