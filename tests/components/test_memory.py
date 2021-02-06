c1eeb2a60c5df2d962933c3bb1c44c12d6e422c8
viach <viachesl@github.com>
2021-02-06 10:36:18 +0300

    changed commit acording to suggestions

--- tests/components/test_memory.py
+++ tests/components/test_memory.py
@@ -2,7 +2,7 @@ from typing import Dict
 import numpy as np
 import math
 
-from sequence.components.memory import Memory, MemoryArray
+from sequence.components.memory import Memory, MemoryArray, MemoryWithRandomCoherenceTime
 from sequence.kernel.event import Event
 from sequence.kernel.process import Process
 from sequence.kernel.timeline import Timeline
@@ -186,13 +186,13 @@ def test_Memory__schedule_expiration():
             counter += 1
     assert counter == 1
     
-def test_Memory__schedule_expiration_random():
+def test_MemoryWithRandomCoherenceTime__schedule_expiration():
     NUM_TRIALS = 200
     coherence_period_avg = 1
     coherence_period_stdev = 0.15
     tl = Timeline()
-    mem = Memory("mem", tl, fidelity=1, frequency=0, efficiency=1, 
-                 coherence_time=(coherence_period_avg,coherence_period_stdev), 
+    mem = MemoryWithRandomCoherenceTime("mem", tl, fidelity=1, frequency=0, efficiency=1, 
+                 coherence_time=coherence_period_avg, coherence_time_stdev=coherence_period_stdev, 
                  wavelength=500)
     parent = DumbParent(mem)
     
@@ -204,7 +204,7 @@ def test_Memory__schedule_expiration_random():
     times_of_expiration_calculated.pop(0)
     
     np.random.seed(2)
-    process = Process(mem, "update_state", [2 * [complex(math.sqrt(1/2))]])
+    process = Process(mem, "update_state", [[complex(math.sqrt(1/2)), complex(math.sqrt(1/2))]])
     for i in range(NUM_TRIALS):
         event = Event(tl.now(), process)
         tl.schedule(event)        
@@ -212,18 +212,18 @@ def test_Memory__schedule_expiration_random():
         tl.run()
         assert times_of_expiration_calculated[i] == tl.now()
         
-    sumX = times_of_expiration_calculated[0]
-    sumXX = times_of_expiration_calculated[0]**2
+    period_sum = times_of_expiration_calculated[0]
+    period_squared_sum = times_of_expiration_calculated[0]**2
     for i in range( 1, len( times_of_expiration_calculated ) ):
-        x = times_of_expiration_calculated[i]-times_of_expiration_calculated[i-1]
-        sumX += x
-        sumXX += x*x
+        period = times_of_expiration_calculated[i]-times_of_expiration_calculated[i-1]
+        period_sum += period
+        period_squared_sum += period*period
     
-    avg_simulated = sumX / NUM_TRIALS * 1e-12
-    stdev_simulated = np.sqrt( ( sumXX - sumX * sumX * 1.0/NUM_TRIALS ) / NUM_TRIALS )*1e-12
-
+    avg_simulated = period_sum / NUM_TRIALS * 1e-12
+    stdev_simulated = np.sqrt( ( period_squared_sum - period_sum * period_sum * 1.0/NUM_TRIALS ) / NUM_TRIALS )*1e-12
 
     #check that values in series are different
     assert stdev_simulated > 0.0
     #probability of error below is less then 0.3%
     assert abs( avg_simulated - coherence_period_avg ) < 3 * coherence_period_stdev / np.sqrt( NUM_TRIALS )
+
