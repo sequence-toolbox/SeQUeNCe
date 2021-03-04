7d34d93273fa0577e51cb48a8c253b3ca2f0c582
Xiaoliang Wu <zurielibra@gmail.com>
2021-03-03 20:40:59 -0800

    add argument about the number of clients to the script qm_server.py

--- mpi_tests/qm_server.py
+++ mpi_tests/qm_server.py
@@ -1,11 +1,14 @@
-from sequence.kernel.quantum_manager_server import generate_arg_parser, start_server
-from pickle import dumps, loads
-import multiprocessing
-from socket import socket
+from sequence.kernel.quantum_manager_server import start_server, valid_ip, \
+    valid_port
+import argparse
 
 if __name__ == '__main__':
-    parser = generate_arg_parser()
+    parser = argparse.ArgumentParser(
+        description='The server of quantum manager')
+    parser.add_argument('ip', type=valid_ip, help='listening IP address')
+    parser.add_argument('port', type=valid_port, help='listening port number')
+    parser.add_argument('client_num', type=int,
+                        help='The number of connected clients')
     args = parser.parse_args()
 
-    start_server(args.ip, args.port)
-
+    start_server(args.ip, args.port, args.client_num)
