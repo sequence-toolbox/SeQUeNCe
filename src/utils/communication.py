186a3230c2896667a73143add32bb5c86ddf15aa
Xiaoliang Wu <zurielibra@gmail.com>
2021-03-31 16:59:56 -0700

    according to the length of data, the function "recv_msg_with_length" aggregates received data if "socket.recv()" function does not get all data

--- src/utils/communication.py
+++ src/utils/communication.py
@@ -18,6 +18,12 @@ def send_msg_with_length(socket: "socket", msg: Any):
 def recv_msg_with_length(socket: "socket") -> Any:
     length_byte = socket.recv(LEN_BYTE_LEN)
     length = int.from_bytes(length_byte, BYTE_ORDER)
-    received_data = socket.recv(length)
-    received_msg = loads(received_data)
+    all_data = ''
+    while len(all_data) < length:
+        received_data = socket.recv(length)
+        if all_data == '':
+            all_data = received_data
+        else:
+            all_data += received_data
+    received_msg = loads(all_data)
     return received_msg
