f1a3c70a1966e15af2c60301c75f68f38f9d9dd7
Xiaoliang Wu <zurielibra@gmail.com>
2021-09-18 23:52:54 -0700

    add function Entity.get_generator() and related tests

---
+++ tests/kernel/test_entity.py
@@ -0,0 +1,32 @@
+from sequence.kernel.entity import Entity
+from sequence.kernel.timeline import Timeline
+from numpy.random import default_rng
+from numpy.random._generator import Generator
+
+
+class FakeOwner():
+    def get_generator(self):
+        return self.generator
+
+
+class Foo(Entity):
+    def init(self):
+        pass
+
+
+def test_get_generator():
+    tl = Timeline()
+
+    # owner does not have generator
+    owner = FakeOwner()
+    foo = Foo("foo", tl)
+    foo.owner = owner
+    assert isinstance(foo.get_generator(), Generator)
+
+    # owner has generator
+    rng = default_rng()
+    owner = FakeOwner()
+    owner.generator = rng
+    foo = Foo("foo", tl)
+    foo.owner = owner
+    assert foo.get_generator() == rng
