3d6646b3eb60397ad9a529453a62246b55d900fe
Alex-Kolar <alexkolar@comcast.net>
2022-01-04 14:18:53 -0600

    Remove Async timeline from p_timeline.py and router_net_topo.py

--- src/kernel/p_timeline.py
+++ src/kernel/p_timeline.py
@@ -47,12 +47,7 @@ class ParallelTimeline(Timeline):
         self.event_buffer = [[] for _ in range(MPI.COMM_WORLD.Get_size())]
         self.lookahead = lookahead
         if qm_ip is not None and qm_port is not None:
-            self.quantum_manager = QuantumManagerClient(formalism, qm_ip,
-                                                        qm_port)
-
-        self.async_tl = AsyncParallelTimeline(lookahead, self.quantum_manager,
-                                              stop_time, formalism)
-        self.async_entities = set()
+            self.quantum_manager = QuantumManagerClient(formalism, qm_ip, qm_port)
 
         self.show_progress = False
 
@@ -66,17 +61,12 @@ class ParallelTimeline(Timeline):
     def schedule(self, event: 'Event'):
         """Method to schedule an event."""
 
-        if type(event.process.owner) is str:
-            if event.process.owner in self.foreign_entities:
-                if event.process.owner not in self.async_entities:
-                    self.buffer_min_ts = min(self.buffer_min_ts, event.time)
-                tl_id = self.foreign_entities[event.process.owner]
-                self.event_buffer[tl_id].append(event)
-                self.schedule_counter += 1
-            elif event.process.owner in self.async_tl.entities:
-                self.async_tl.import_event(event)
-            else:
-                super(ParallelTimeline, self).schedule(event)
+        if type(event.process.owner) is str \
+                and event.process.owner in self.foreign_entities:
+            self.buffer_min_ts = min(self.buffer_min_ts, event.time)
+            tl_id = self.foreign_entities[event.process.owner]
+            self.event_buffer[tl_id].append(event)
+            self.schedule_counter += 1
         else:
             super(ParallelTimeline, self).schedule(event)
 
@@ -95,8 +85,7 @@ class ParallelTimeline(Timeline):
     def run(self):
         while self.time < self.stop_time:
             tick = time()
-            min_time = min(self.buffer_min_ts, self.top_time(),
-                           self.async_tl.top_time())
+            min_time = min(self.buffer_min_ts, self.top_time())
             for buf in self.event_buffer:
                 buf.append(min_time)
             inbox = MPI.COMM_WORLD.alltoall(self.event_buffer)
@@ -123,9 +112,6 @@ class ParallelTimeline(Timeline):
             self.time = min_time
 
             tick = time()
-            events = self.async_tl.run(sync_time)
-            for event in events:
-                self.schedule(event)
             while len(self.events) > 0 and self.events.top().time < sync_time:
                 event = self.events.pop()
                 if event.is_invalid():
@@ -147,48 +133,3 @@ class ParallelTimeline(Timeline):
         """
 
         self.foreign_entities[entity_name] = foreign_id
-
-    def move_entity_to_async_tl(self, entity_name: str):
-        self.async_tl.entities[entity_name] = self.entities[entity_name]
-        self.entities[entity_name].change_timeline(self.async_tl)
-        del self.entities[entity_name]
-
-
-class AsyncParallelTimeline(Timeline):
-    def __init__(self, lookahead, quantum_manager, stop_time=float('inf'),
-                 formalism='KET'):
-        super().__init__(stop_time, formalism)
-        self.quantum_manager = quantum_manager
-        self.lookahead = lookahead
-        self.exchange_counter = 0
-        self.computing_time = 0
-        self.new_events = []
-
-    def top_time(self):
-        if len(self.events) > 0:
-            return self.events.top().time + self.lookahead
-        else:
-            return float('inf')
-
-    def run(self, stop_time: int) -> List["Event"]:
-        self.new_events = []
-        tick = time()
-        while len(self.events) > 0 and self.events.top().time < stop_time:
-            event = self.events.pop()
-            if event.is_invalid():
-                continue
-            assert self.time <= event.time, "invalid event time for process scheduled on " + str(
-                event.process.owner)
-            self.time = event.time
-            event.process.run()
-            self.run_counter += 1
-        self.computing_time += time() - tick
-        return self.new_events
-
-    def schedule(self, event: "Event") -> None:
-        self.new_events.append(event)
-
-    def import_event(self, event: "Event"):
-        if type(event.process.owner) == str:
-            event.process.owner = self.get_entity_by_name(event.process.owner)
-        self.events.push(event)
