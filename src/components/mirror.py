440310b6050c7667b2c2eb6164516490f1fd0393
Maria Gragera Garces <m.gragera.garces@gmail.com>
2022-03-20 13:34:12 +0000

    Update mirror.py

--- src/components/mirror.py
+++ src/components/mirror.py
@@ -1,3 +1,17 @@
+from numpy import random, multiply
+
+from .photon import Photon
+from ..kernel.entity import Entity
+from ..kernel.event import Event
+from ..kernel.process import Process
+from ..utils.encoding import polarization
+
+from typing import TYPE_CHECKING, Any, Dict
+if TYPE_CHECKING:
+    from ..kernel.timeline import Timeline
+    from ..components.photon import Photon
+    from typing import List
+
 class Mirror(Entity):
     
     """Single photon reflecting device.
