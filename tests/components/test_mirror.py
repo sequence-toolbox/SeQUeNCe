96c4143a100db444137dc281e6893bfd41d7821f
Alex-Kolar <alexkolar@comcast.net>
2022-06-27 15:50:37 -0500

    Modifications to mirror.py

--- tests/components/test_mirror.py
+++ tests/components/test_mirror.py
@@ -15,10 +15,6 @@ from sequence.components.light_source import LightSource
 
 from sequence.components.mirror import Mirror
 
-NUM_TRIALS = 1000
-FREQUENCY = 1e3
-SEED = 1
-
 
 class Counter:
     def __init__(self):
@@ -29,35 +25,34 @@ class Counter:
 
 
 class EmittingNode(Node):
-    def __init__(self, name, timeline):
+    def __init__(self, name, timeline, light_source):
         super().__init__(name, timeline)
-
-        ls_name = name + ".ls"
-        self.light_source = LightSource(ls_name, timeline, frequency=int(8e7), mean_photon_num=1)
+        self.light_source = light_source
         self.light_source.owner = self
 
 
 class MiddleNode(Node):
-    def __init__(self, name, timeline, mirror, receiver):
+    def __init__(self, name, timeline, mirror):
         super().__init__(name, timeline)
         self.mirror = mirror
         self.mirror.owner = self
-        self.receiver = receiver
 
     def receive_qubit(self, src, qubit):
-        # print("received something")
-        if not qubit.is_null:
-            self.mirror.get()
+        self.mirror.get(qubit)
 
-            if self.get_generator().random() < self.mirror.fidelity:
-                process_photon = Process(self.mirror, "emit", [
-                                         [qubit.quantum_state.state], self.receiver])
-
-                time = self.timeline.now()
-                period = int(round(1e12 / self.mirror.frequency))
-                event = Event(time, process_photon)
-                self.owner.timeline.schedule(event)
-                time += period
+        # # print("received something")
+        # if not qubit.is_null:
+        #     self.mirror.get()
+        #
+        #     if self.get_generator().random() < self.mirror.fidelity:
+        #         process_photon = Process(self.mirror, "emit", [
+        #                                  [qubit.quantum_state.state], self.receiver])
+        #
+        #         time = self.timeline.now()
+        #         period = int(round(1e12 / self.mirror.frequency))
+        #         event = Event(time, process_photon)
+        #         self.owner.timeline.schedule(event)
+        #         time += period
 
 
 class Receiver(Node):
@@ -70,18 +65,24 @@ class Receiver(Node):
 
 
 def test_mirror():
+    STATE_LEN = 1000
+    FIDELITY = 0.98
+    LS_FREQ = 8e7
+    MEAN = 0.1
+
     tl = Timeline()
-    FID, FREQ, MEAN = 0.98, 8e7, 0.1
 
-    sender = EmittingNode("sender", tl)
+    ls = LightSource("ls", tl, frequency=LS_FREQ, mean_photon_num=MEAN)
+    sender = EmittingNode("sender", tl, ls)
     receiver = Receiver("receiver", tl)
-    mr = Mirror("mr", tl, fidelity=FID, frequency=8e7, mean_photon_num=MEAN)
-    mid = MiddleNode("mid", tl, mr, receiver.name)
-    mid.set_seed(SEED)
+    mr = Mirror("mr", tl, fidelity=FIDELITY, destination=receiver.name)
+    mid = MiddleNode("mid", tl, mr)
 
-    assert mid.mirror.fidelity == FID
-    assert mid.mirror.frequency == FREQ
-    assert mid.mirror.mean_photon_num == MEAN
+    sender.set_seed(0)
+    mid.set_seed(1)
+    receiver.set_seed(2)
+
+    assert mid.mirror.fidelity == FIDELITY
 
     qc1 = QuantumChannel("qc1", tl, distance=1e5, attenuation=0)
     qc2 = QuantumChannel("qc2", tl, distance=1e5, attenuation=0)
@@ -89,9 +90,8 @@ def test_mirror():
     qc2.set_ends(mid, receiver.name)
 
     state_list = []
-    STATE_LEN = 1000
+    rng = mid.get_generator()
     for _ in range(STATE_LEN):
-        rng = mid.get_generator()
         basis = rng.integers(2)
         bit = rng.integers(2)
         state_list.append(polarization["bases"][basis][bit])
@@ -100,9 +100,8 @@ def test_mirror():
     sender.light_source.emit(state_list, "mid")
     tl.run()
 
-    assert abs((len(receiver.log) / STATE_LEN) - MEAN) < 0.1
+    assert abs((len(receiver.log) / STATE_LEN) - (MEAN * FIDELITY)) < 0.1
     for time, src, qubit in receiver.log:
         index = int(qubit.name)
         assert state_list[index] == qubit.quantum_state.state
-        assert time == index * (1e12 / FREQ) + qc1.delay + qc2.delay
-        assert src == "sender"
+        assert time == index * (1e12 / LS_FREQ) + qc1.delay + qc2.delay
