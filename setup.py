ac774abda9981635584beaccac52a70d5a0484c6
Alex-Kolar <alexkolar@comcast.net>
2022-07-08 11:11:45 -0500

    Update setup.py to match RnD

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="sequence",
-    version="0.2.3",
+    version="0.3.2",
     author="Xiaoliang Wu, Joaquin Chung, Alexander Kolar, Alexander Kiefer, Eugene Wang, Tian Zhong, Rajkumar Kettimuthu, Martin Suchara",
     author_email="xwu64@hawk.iit.edu, chungmiranda@anl.gov, akolar@anl.gov, akiefer@iu.edu, eugenewang@yahoo.com, tzh@uchicago.edu, kettimut@mcs.anl.gov, msuchara@anl.gov",
     description="Simulator of Quantum Network Communication: SEQUENCE-Python is a prototype version of the official SEQUENCE release.",
@@ -13,18 +13,20 @@ setup(
     package_dir={'sequence': 'src'},
     include_package_data=True,
     install_requires=[
-        'numpy',
+        'numpy<1.22',
 	    'matplotlib',
-        'json5',
         'pandas',
-        'qutip',
+        'qutip>=4.6.0',
         'dash>=1.20.0',
         'dash-core-components',
         'dash-html-components',
         'dash-bootstrap-components',
+        'tqdm>=4.54.0',
         'networkx',
         'dash-table',
         'dash-cytoscape',
         'plotly',
+        'mpi4py',
+        'pytest-mpi',
     ],
 )
