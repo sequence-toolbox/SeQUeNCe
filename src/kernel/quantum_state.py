8e22b949f0c2d1042245fc030253513896f0b60b
allenyxzang <yzang@uchicago.edu>
2023-07-24 15:06:40 -0500

    syntax bug fix
    Add missing left [

--- src/kernel/quantum_state.py
+++ src/kernel/quantum_state.py
@@ -407,7 +407,7 @@ class BellDiagonalState(State):
         super().__init__()
 
         # check formatting
-        assert all(elem <= 1.001 and elem >= 0 for elem in diag_elems]), "Illegal value with elem > 1 or elem < 0 in density matrix diagonal elements"
+        assert all([elem <= 1.001 and elem >= 0 for elem in diag_elems]), "Illegal value with elem > 1 or elem < 0 in density matrix diagonal elements"
         assert abs(sum([elem for elem in diag_elems]) - 1) < 1e-5, "Density matrix diagonal elements do not sum to 1"
 
         self.state = array(diag_elems, dtype=float)  # density matrix diagonal elements are guaranteed to be real from Hermiticity
