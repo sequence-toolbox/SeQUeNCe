21276599f097aa112c639ae12d3136c6a1f305e4
Alex-Kolar <alexkolar@comcast.net>
2023-08-21 17:40:00 -0500

    Add single heralded generation to test_generation.py

--- tests/entanglement_management/test_generation.py
+++ tests/entanglement_management/test_generation.py
@@ -72,11 +72,12 @@ def test_generation_receive_message():
 
 
 def test_generation_pop():
-    class DumbBSM():
+    class DumbBSM:
         def __init__(self):
             self.resolution = 1
+            self.encoding = "single_atom"
 
-    class DumbNode():
+    class DumbNode:
         def __init__(self):
             self.name = "none"
             self.protocols = []
@@ -309,4 +310,86 @@ def test_generation_fidelity_ket():
     assert total > 0, "More trials needed; insufficient successes"
     ratio = correct / total
     assert abs(ratio - FIDELITY) < 0.1
+
+
+def test_generation_single_herald():
+    NUM_TESTS = 100
+
+    tl = Timeline()
+
+    e0 = FakeRouter("e0", tl)
+    m0 = FakeBSMNode("m0", tl)
+    e1 = FakeRouter("e1", tl)
+    e0.set_seed(0)
+    m0.set_seed(1)
+    e1.set_seed(2)
+
+    # add connections
+    qc0 = QuantumChannel("qc_e0m0", tl, 0, 1e3)
+    qc1 = QuantumChannel("qc_e1m0", tl, 0, 1e3)
+    qc0.set_ends(e0, m0.name)
+    qc1.set_ends(e1, m0.name)
+
+    for src in [e0, e1, m0]:
+        for dst in [e0, e1, m0]:
+            if src.name != dst.name:
+                cc = ClassicalChannel("cc_%s_%s" % (src.name, dst.name), tl,
+                                      1e3, delay=1e9)
+                cc.set_ends(src, dst.name)
+
+    # add hardware
+    e0.memory_array = MemoryArray("e0.memory_array", tl, num_memories=NUM_TESTS)
+    e0.memory_array.owner = e0
+    e1.memory_array = MemoryArray("e1.memory_array", tl, num_memories=NUM_TESTS)
+    e1.memory_array.owner = e1
+    detectors = [{"efficiency": 1, "count_rate": 1e11}] * 2
+    m0.bsm = make_bsm("m0.bsm", tl, encoding_type="single_atom", detectors=detectors)
+    m0.bsm.owner = m0
+
+    # add middle protocol
+    eg_m0 = EntanglementGenerationB(m0, "eg_m0", others=["e0", "e1"])
+    m0.bsm.attach(eg_m0)
+
+    tl.init()
+
+    protocols_e0 = []
+    protocols_e1 = []
+
+    for i in range(NUM_TESTS):
+        name0, name1 = [f"eg_e{j}[{i}]" for j in range(2)]
+        protocol0 = EntanglementGenerationA(e0, name0, middle="m0", other="e1", memory=e0.memory_array[i],
+                                            is_sh=True)
+        e0.protocols.append(protocol0)
+        protocols_e0.append(protocol0)
+        protocol1 = EntanglementGenerationA(e1, name1, middle="m0", other="e0", memory=e1.memory_array[i],
+                                            is_sh=True)
+        e1.protocols.append(protocol1)
+        protocols_e1.append(protocol1)
+        protocol0.set_others(protocol1.name, e1.name, [e1.memory_array[i].name])
+        protocol1.set_others(protocol0.name, e0.name, [e0.memory_array[i].name])
+
+        for protocol in [protocols_e0[i], protocols_e1[i]]:
+            process = Process(protocol, "start", [])
+            event = Event(i * 1e12, process)
+            tl.schedule(event)
+
+    tl.run()
+
+    assert len(e0.resource_manager.log) == NUM_TESTS
+    assert len(e1.resource_manager.log) == NUM_TESTS
+    empty_count = 0
+    for i in range(NUM_TESTS):
+        if e0.resource_manager.log[i][1] == "RAW":
+            empty_count += 1
+        else:
+            assert e0.resource_manager.log[i][1] == "ENTANGLED"
+            memory0 = e0.resource_manager.log[i][0]
+            memory1 = e1.resource_manager.log[i][0]
+            assert memory0.fidelity == memory0.raw_fidelity
+            assert memory1.fidelity == memory1.raw_fidelity
+            assert memory0.entangled_memory["node_id"] == e1.name
+            assert memory1.entangled_memory["node_id"] == e0.name
+
+    ratio = empty_count / NUM_TESTS
+    assert abs(ratio - 0.5) < 0.1
 
