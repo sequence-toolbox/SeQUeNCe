62ef4f45c776453ac838dd7df58a5d6698291e0b
alvinquantum <agonzales@anl.gov>
2023-09-05 14:19:19 -0500

    Add class with partial interfaces for initializing the application in qutip_integration.py
    Moved existing interface functions into the AppQutipInt class

--- example/qutip_integration.py
+++ example/qutip_integration.py
@@ -1,30 +1,41 @@
 import qutip as qtp
 
-def get_memory(mem_info):
-    '''Starts the operations on the bell state.
-    args: mem_info : : resource_management.memory_manager.MemoryInfo'''
-    pass
+class AppQutipInt:
+    def __init__(self) -> None:
+        pass
 
-def bds_to_qobj(bell_st):
-    '''Converts bell_st to qutip qobj.
-    args: bell_st : : 
-    
-    Returns: qtp.qobj'''
-    pass
+    def start(self):
+        pass
 
-def start_decoherence():
-    '''Asynch process that modifies stored qobj with decoherence.'''
-    pass
+    def get_memory(self, mem_info):
+        '''Starts the operations on the bell state.
+        args: mem_info : : resource_management.memory_manager.MemoryInfo'''
+        pass
+
+    def bds_to_qobj(self, bell_st):
+        '''Converts bell_st to qutip qobj.
+        args: bell_st : : 
+        
+        Returns: qtp.qobj'''
+        pass
+
+    def start_decoherence(self):
+        '''Asynch process that modifies stored qobj with decoherence.'''
+        pass
 
 
 if __name__ == "__main__":
+    # Start application.
+    # Sequence stuff to init.
+    app = AppQutipInt()
+    app.start()
     # Get from sequence.
-    mem_info=[]
-    get_memory(mem_info)
+    # mem_info=[]
+    # app.get_memory(mem_info)
 
-    # Get from sequence.
-    bell_st=[] 
-    bds_to_qobj(bell_st)
+    # # Get from sequence.
+    # bell_st=[] 
+    # app.bds_to_qobj(bell_st)
 
-    start_decoherence()
-    #Begin gate teleporation.
+    # app.start_decoherence()
+    # #Begin gate teleporation.
