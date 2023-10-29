448d51dbfa4cef9bae0e426d4a6c881e303c6763
Caitao Zhan <caitaozhan@gmail.com>
2023-10-28 22:23:29 -0400

    minor updates in tutorial: 1) In chapter3/example1.py, add import for Message and ignore classical channel from a node to itself, which do not make sense; 2) In chapter3/example3.py, also ignore classical channel from a node to itself; 3) In entanglement_management/swapping.py, fix minor comment issue in EntanglementSwappingB

--- docs/source/tutorial/chapter3/example1.py
+++ docs/source/tutorial/chapter3/example1.py
@@ -3,6 +3,7 @@ from sequence.topology.node import Node, BSMNode
 from sequence.components.memory import Memory
 from sequence.components.optical_channel import QuantumChannel, ClassicalChannel
 from sequence.entanglement_management.generation import EntanglementGenerationA
+from sequence.message import Message
 
 
 class SimpleManager:
@@ -76,9 +77,10 @@ nodes = [node1, node2, bsm_node]
 
 for i in range(3):
     for j in range(3):
-        cc = ClassicalChannel('cc_%s_%s' % (nodes[i].name, nodes[j].name), tl,
-                              1000, 1e8)
-        cc.set_ends(nodes[i], nodes[j].name)
+        if i != j:
+            cc = ClassicalChannel('cc_%s_%s' % (nodes[i].name, nodes[j].name), tl,
+                                1000, 1e8)
+            cc.set_ends(nodes[i], nodes[j].name)
 
 tl.init()
 for i in range(1000):
