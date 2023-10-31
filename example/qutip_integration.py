2152c3db10b8ddd897d9331c159cfe6cf9e726db
alvinquantum <agonzales@anl.gov>
2023-10-31 13:04:52 -0500

    Added to /example/qutip_integration.py
    added to get_memory and bds_to_qobj methods in AppQutipInt

--- example/qutip_integration.py
+++ example/qutip_integration.py
@@ -1,23 +1,33 @@
 import qutip as qtp
+import numpy as np
+import sequence as sqnc
+from sequence.resource_management.memory_manager import MemoryManager 
+
 
 class AppQutipInt:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, mem_manager: MemoryManager) -> None:
+        '''Args:
+            mem_manager (MemoryManager): manages all the memories'''
+        self.mem_manager=mem_manager
 
-    def start(self):
+    def start(self) -> None:
         pass
 
     def get_memory(self, mem_info):
         '''Starts the operations on the bell state.
-        args: mem_info : : resource_management.memory_manager.MemoryInfo'''
-        pass
+        Args: 
+            mem_info (resource_management.memory_manager.MemoryInfo): '''
+        todo: "Alvin: I'm not sure what method will determine the memory state."
+        state="ENTANGLED"
+        self.mem_manager.update(mem_info.memory, state)
 
     def bds_to_qobj(self, bell_st):
         '''Converts bell_st to qutip qobj.
-        args: bell_st : : 
-        
+        Args: 
+            bell_st (np.ndarray) : 
         Returns: qtp.qobj'''
-        pass
+        return qtp.Qobj(bell_st)
+        
 
     def start_decoherence(self):
         '''Asynch process that modifies stored qobj with decoherence.'''
@@ -27,7 +37,8 @@ class AppQutipInt:
 if __name__ == "__main__":
     # Start application.
     # Sequence stuff to init.
-    app = AppQutipInt()
+    mem_manager=[]
+    app = AppQutipInt(mem_manager)
     app.start()
     # Get from sequence.
     # mem_info=[]
@@ -39,3 +50,8 @@ if __name__ == "__main__":
 
     # app.start_decoherence()
     # #Begin gate teleporation.
+
+    # Tests
+    state=np.array([[0,1],[1,0]])
+    print(state)
+    print(app.bds_to_qobj(state))
