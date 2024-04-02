c1eedd2965df451c41b0d55719e79f6d407ba69f
Alex-Kolar <alexkolar@comcast.net>
2024-04-02 12:16:34 -0500

    Add formalism argument to topology construction

--- src/topology/router_net_topo.py
+++ src/topology/router_net_topo.py
@@ -4,6 +4,7 @@ from numpy import mean
 
 from .topology import Topology as Topo
 from ..kernel.timeline import Timeline
+from ..kernel.quantum_manager import KET_STATE_FORMALISM
 from .node import BSMNode, QuantumRouter
 
 
@@ -59,10 +60,11 @@ class RouterNetTopo(Topo):
 
     def _add_timeline(self, config):
         stop_time = config.get(Topo.STOP_TIME, float('inf'))
+        formalism = config.get(Topo.FORMALISM, KET_STATE_FORMALISM)
         if config.get(self.IS_PARALLEL, False):
             raise Exception("Please install 'psequence' package for parallel simulations.")
         else:
-            self.tl = Timeline(stop_time)
+            self.tl = Timeline(stop_time, formalism=formalism)
 
     def _map_bsm_routers(self, config):
         for qc in config[Topo.ALL_Q_CHANNEL]:
