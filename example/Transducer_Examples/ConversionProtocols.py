5b5da7611e23d7f8963b385ee5688b8fc4510c16
Laura d'Avossa <lauradavossa@gmail.com>
2025-04-10 18:00:20 +0200

    updates

--- example/Transducer_Examples/ConversionProtocols.py
+++ example/Transducer_Examples/ConversionProtocols.py
@@ -58,8 +58,8 @@ class EmittingProtocol(Protocol):
     def start(self):
         print(f"EmittingProtocol started for {self.owner.name} at time {self.tl.now()}")
         photon = self.transmon.generation()
-        print(f"Photon created: {photon}, Name: {photon. name}, Wavelength: {photon.wavelength}")
-        print(f"Transmon at Tx quantum state: {self.transmon.input_quantum_state} of of {self.owner.name}")
+        print(f"Photon created: {photon}, Name: {photon.name}, Wavelength: {photon.wavelength}")
+        print(f"Transmon at Tx quantum state: {self.transmon.input_quantum_state} of {self.owner.name}")
 
         if self.transmon.photons_quantum_state[0] == KET1:
             if random.random() < self.transmon.efficiency:
@@ -86,8 +86,6 @@ class UpConversionProtocol(Protocol):
         transmon (Transmon): the transmon component
     """
 
-    #def __init__(self, owner: Node, name: str, tl: Timeline, transducer: Transducer, transmon: Transmon):
-    #ITALIANO: qui ho deciso di non usare il trasmon
     def __init__(self, owner: Node, name: str, tl: Timeline, transducer: Transducer):
 
         super().__init__(owner, name)
@@ -95,7 +93,6 @@ class UpConversionProtocol(Protocol):
         self.name = name
         self.tl = tl
         self.transducer = transducer
-        #self.transmon = transmon
 
     def start(self) -> None:
         """start the protocol  
@@ -108,47 +105,20 @@ class UpConversionProtocol(Protocol):
         """
 
     def convert(self, photon) -> None:
+        
 
-        #if self.transducer.photon_counter > 0:   # NOTE shouldn't use this photon_counter to determine
-            #custom_gate = get_conversion_matrix(self.transducer.efficiency)
+        print(f"Transducer first receiver: {self.transducer._receivers[0]}")
+        print(f"Transducer second receiver: {self.transducer._receivers[1]}")
 
-            #transmon_state_vector = np.array(self.transmon.input_quantum_state).reshape((4, 1))
-            #photon_state = Qobj(transmon_state_vector, dims=[[4], [1]])
-
-            #new_photon_state = custom_gate * photon_state
-            #self.transducer.quantum_state = new_photon_state.full().flatten()
-
-            #print(f"Transducer at Tx quantum state: {self.transducer.quantum_state}")
-
-            print(f"Transducer first receiver: {self.transducer._receivers[0]}")
-            print(f"Transducer second receiver: {self.transducer._receivers[1]}")
-
-            if random.random() < self.transducer.efficiency:
-                photon.wavelength = OPTICAL_WAVELENGTH
-                print("Successful up-conversion")
-                print(f"The photon is: {photon} with wavelength: {photon.wavelength}")
-                #self.transducer.output_quantum_state = [0.0 + 0.0j, 0.0 + 0.0j, 1.0 + 0.0j, 0.0 + 0.0j]
-                #print(f"State after successful up-conversion: {self.transducer.output_quantum_state}")
-                print(f"Transducer receiver: {self.transducer._receivers[0]}")
-                self.transducer._receivers[0].transmit(photon) 
-                
-                #self.transducer._receivers[0].receive_qubit(photon, [self.node.name])
-
-                # #ITALIANO: QUESTOOOOOO
-                #self.transducer._receivers[0].transmit(photon)
-                #self.transducer._receivers[0].transmit(photon, ["node"])  # Passa una lista con il nome del nodo sorgente
-
-
-
-                
-                #self.transducer._receivers[0].receive_photon(self.node, photon)  # NOTE the receiver should be the quantum channel OK DONE
-            
-            else:
-                photon.wavelength = MICROWAVE_WAVELENGTH
-                self.transducer._receivers[1].get(photon)
-                print("FAILED up-conversion")
-        #else:
-        #    print("No photon to up-convert")
+        if random.random() < self.transducer.efficiency:
+            photon.wavelength = OPTICAL_WAVELENGTH
+            print("Successful up-conversion")
+            print(f"The photon is: {photon} with wavelength: {photon.wavelength} at time {self.tl.now()}")
+            self.transducer._receivers[0].transmit(photon)
+        else:
+            photon.wavelength = MICROWAVE_WAVELENGTH
+            self.transducer._receivers[1].get(photon)
+            print("FAILED up-conversion")
 
     def received_message(self, src: str, msg):
         pass
@@ -180,37 +150,19 @@ class DownConversionProtocol(Protocol):
             photon (Photon): the photon received at the transducer from the quantum channel
         """
     def convert(self, photon) -> None:
-        
-        #if self.transducer.photon_counter > 0:   # NOTE shouldn't use this photon_counter to determine
-
-            #transducer_state = [0.0 + 0.0j, 0.0 + 0.0j, 1.0 + 0.0j, 0.0 + 0.0j]  # NOTE Why is this the transducer's state 
-            #custom_gate = get_conversion_matrix(self.transducer.efficiency)
-
-            #transducer_state_vector = np.array(transducer_state).reshape((4, 1))
-            #transducer_state = Qobj(transducer_state_vector, dims=[[4], [1]])
-
-            #new_transducer_state = custom_gate * transducer_state
-            #self.transducer.quantum_state = new_transducer_state.full().flatten()
-
-            #print(f"Transducer at Rx quantum state: {self.transducer.quantum_state}")
-
-            if random.random() < self.transducer.efficiency:
-                photon.wavelength = MICROWAVE_WAVELENGTH
-                print("Successful down-conversion")
-                print(f"The photon is: {photon} with wavelength: {photon.wavelength}")
-                print(f"Transducer receiver: {self.transducer._receivers[0]}")
-                
-                self.transducer._receivers[0].get(photon)
-
-
-                #self.transducer.output_quantum_state = [0.0 + 0.0j, 0.0 + 0.0j, 1.0 + 0.0j, 0.0 + 0.0j]
-                #print(f"State after successful down-conversion: {self.transducer.output_quantum_state}")
-            else:
-                photon.wavelength = OPTICAL_WAVELENGTH
-                self.transducer._receivers[1].get(photon)
-                print("FAILED down-conversion")
-        #else:
-        #    print("No photon to down-convert")
+       
+        print(f"PHOTON IS: {photon}")
+        if random.random() < self.transducer.efficiency:
+            photon.wavelength = MICROWAVE_WAVELENGTH
+            print("Successful down-conversion")
+            print(f"The photon is: {photon} with wavelength: {photon.wavelength}")
+            print(f"Transducer receiver: {self.transducer._receivers[0]}")
+            self.transducer._receivers[0].get(photon)
+        else:
+            photon.wavelength = OPTICAL_WAVELENGTH
+            self.transducer._receivers[1].get(photon)
+            print("FAILED down-conversion")
+            print(f"Transducer receiver: {self.transducer._receivers[1]}")
 
     def received_message(self, src: str, msg):
         pass
--- example/Transducer_Examples/SwappingProtocols.py
+++ example/Transducer_Examples/SwappingProtocols.py
@@ -21,27 +21,6 @@ OPTICAL_WAVELENGTH = 1550 # nm
 
 
 
-class EmittingProtocol(Protocol):
-
-    """Protocol for emission of single microwave photon by transmon"""
-
-    def __init__(self, own: Node, name: str, tl: Timeline, transmon: Transmon, transducer: Transducer):
-        super().__init__(own, name)
-        self.owner = own
-        self.name = name
-        self.tl = tl
-        self.transmon = transmon
-        self.transducer = transducer
-
-    def start(self) -> None:
-
-        self.transducer.photon_counter += 1 
-        #print(f"Microwave photons emitted by {self.name}: {self.transducer.photon_counter}")
-
-    def received_message(self, src: str, msg):
-        pass
-
-
 class Swapping(Protocol):
 
     """Entanglement swapping in the middle."""
@@ -52,8 +31,7 @@ class Swapping(Protocol):
         self.name = name
         self.tl = tl
         self.FockBS = FockBS
-        #self.FockBS.delay= delay
-        #self.measurement_protocol = measure_protocol
+       
 
     def start(self, photon: Photon) -> None:
         pass
@@ -72,10 +50,7 @@ class Swapping(Protocol):
         self.spd_ideal= 0 
 
         print("ENTANGLEMENT SWAPPING PROTOCOL STARTS AT time: ", self.tl.now())
-        #print(f"Detector efficiency is: ", self.FockBS._receivers[0].efficiency)
-        #print(f"Detector efficiency is: ", self.FockBS._receivers[1].efficiency)
-
-        #PROVAAAAAAAA
+        
 
        
         if photon_count == 1:
@@ -86,12 +61,6 @@ class Swapping(Protocol):
             selected_receiver.get(photon)
             selected_receiver.get_2(photon) 
 
-            #selected_receiver.photon_counter2=+1 #ideal
-            
-            #if random.random() < selected_receiver.efficiency: #real
-                #selected_receiver.photon_counter += 1 
-            
-            #self.measurement_protocol.measure(photon)  # Chiamata al protocollo measure
 
         elif photon_count == 2:
             print("DUE FOTONI")
@@ -103,25 +72,19 @@ class Swapping(Protocol):
             selected_receiver.get(photon)
             selected_receiver.get_2(photon) 
             selected_receiver.get_2(photon)
-            #future_time = self.timeline.now() + self.FockBS.delay
+           
 
-            future_time = self.tl.now() + 1  # Delay opzionale
-            #QUI CI VUOLE UN IF IN QUALCHE MODO
+            future_time = self.tl.now() + 1  
             process = Process(selected_receiver, "measure", [photon])
             event = Event(future_time, process)
             self.tl.schedule(event)
 
-        #return  self.detector_photon_counter_real, self.spd_real, self.detector_photon_counter_ideal, self.spd_ideal
         
     
     def received_message(self, src: str, msg):
         pass 
     
-        #selected_receiver.photon_counter2=+2 #ideal
-
-        #print("MEASUREMENT PROTOCOL STARTS AT time: ", self.tl.now())
-
-        #ITALIANOOOOOOO
+   
     
 
 
@@ -142,33 +105,8 @@ class Measure(Protocol):
 
     def start(self) -> None:
         
-        print("MEASUREMENT PROTOCOL STARTS AT time: ", self.tl.now())
-        print("--------")
-        #print("CASO DETECTOR IDEALE")
+      
 
-        print("IL PRIMO RICEVITORE è ", self.FockBS._receivers[0])
-        print("IL SECONDO RICEVITORE è ", self.FockBS._receivers[1])
-
-
-        print("il conteggio del PRIMO RICEVITORE è: ", self.FockBS._receivers[0].photon_counter)
-        print("il conteggio del SEONCONDO ricevitore è : ", self.FockBS._receivers[1].photon_counter)
-
-        print("il conteggio del PRIMO RICEVITORE IDEALE: ", self.FockBS._receivers[0].photon_counter2)
-        print("il conteggio del SECONDO RICEVITORE IDEALE: ", self.FockBS._receivers[1].photon_counter2)
-        print("Fock beams photon counter: ", self.FockBS.photon_counter)
-        print("--------")
-
-        #if self.FockBS._receivers[0].photon_counter2 == 0 and self.FockBS._receivers[1].photon_counter2 == 0:
-        #    print("No photon reach the beam splitter")
-        
-
-
-        
-
-
-
-
-    
         if self.FockBS._receivers[0].photon_counter2 == 1 or self.FockBS._receivers[1].photon_counter2 == 1:
            self.detector_photon_counter_ideal += 1
             
@@ -196,17 +134,6 @@ class Measure(Protocol):
         return self.detector_photon_counter_ideal, self.spd_ideal, self.detector_photon_counter_real, self.spd_real 
 
     
-    #def get_detector_photon_counter_ideal(self):
-    #    return self.detector_photon_counter_ideal
-
-    #def get_spd_ideal(self):
-    #    return self.spd_ideal
-    
-    #def get_detector_photon_counter_real(self):
-    #    return self.detector_photon_counter_real
-
-    #def get_spd_real(self):
-    #    return self.spd_real
     
     def received_message(self, src: str, msg):
         pass
