b6a6f6a47e98bbb2b13add2540d9750450891825
ANSH SINGAL <anshsingal1999@gmail.com>
2025-06-13 03:20:14 -0500

    Implemented optical H gate, generalized BS operator and phase shifter.

--- sequence_projects/swap_files/swap_TN_direct.py
+++ sequence_projects/swap_files/swap_TN_direct.py
@@ -111,18 +111,48 @@ def create_BS_MPO(site1, site2, theta, total_sites, N, tag = 'BS'):
     a_dag = a.T
     I = np.eye(N)
     
-    # The V polarization would be transmitted unchanged. So, we only focus on the 
-    # H polarized photons. Now, we have 2 modes of H polarized photons which need to be 
-    # reflected. So, we create the unitaries for both individually and multiply them (not in this function.)
+    # This corresponds to the BS hamiltonian:
 
     hamiltonian_BS = -theta * ( kron(I, a_dag)@kron(a, I) - kron(I, a)@kron(a_dag, I) )
     unitary_BS = expm(hamiltonian_BS)
+
+    print("unitary_BS", unitary_BS)
 
     BS_MPO = mpo.from_dense(unitary_BS, dims = N, sites = (site1,site2), L=total_sites, tags=tag)
     # BS_MPO = BS_MPO.fill_empty_sites(mode = "full")
     return BS_MPO
 
 
+def generalized_mode_mixer(site1, site2, theta, phi, psi, lamda, total_sites, N, tag = 'MM'): 
+
+    a = qt.destroy(N).full()
+    a_dag = a.T
+    I = np.eye(N)
+    
+    # This corresponds to the BS hamiltonian: This is a different difinition from the one in 
+    # create_BS_MPO. This is because of how the generalized beamsplitter is defined in DOI: 10.1088/0034-4885/66/7/203 . 
+    hamiltonian_BS = theta * (kron(a_dag, I)@kron(I, a) + kron(a, I)@kron(I, a_dag))
+    unitary_BS = expm(-1j * hamiltonian_BS)
+
+    # print("unitary_BS\n", np.round(unitary_BS, 4))
+
+    pre_phase_shifter = np.kron(phase_shifter(N, phi[0]/2), phase_shifter(N, phi[1]/2))
+    post_phase_shifter = np.kron(phase_shifter(N, psi[0]/2), phase_shifter(N, psi[1]/2))
+    global_phase_shifter = np.kron(phase_shifter(N, lamda[0]/2), phase_shifter(N, lamda[1]/2))
+
+    # This construction for the generalized beamsplitter is based on the description in paper DOI: 10.1088/0034-4885/66/7/203
+    generalized_BS = global_phase_shifter @  (pre_phase_shifter @ unitary_BS @ post_phase_shifter)
+
+    # print("generalized_BS\n", np.round(generalized_BS, 4))
+
+    BS_MPO = mpo.from_dense(generalized_BS, dims = N, sites = (site1,site2), L=total_sites, tags=tag)
+    # BS_MPO = BS_MPO.fill_empty_sites(mode = "full")
+    return BS_MPO
+
+
+def phase_shifter(N, theta):
+    diag = [np.exp(1j * theta * i) for i in range(N)]
+    return np.diag(diag, k=0)
 
 
 ###### POVM OPERATORS #######
