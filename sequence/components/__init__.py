5f9190e815f6a4d9f8f48cb6f2f795fe01de12c1
Robert Hayek <hayekr2@gmail.com>
2025-07-01 00:24:11 -0500

    remove scrap

--- sequence/components/__init__.py
+++ sequence/components/__init__.py
@@ -2,8 +2,6 @@ import importlib
 __all__ = ['beam_splitter', 'bsm', 'detector', 'interferometer', 'light_source', 'memory', 'optical_channel', 'photon',
            'spdc_lens', 'switch', 'circuit', 'transmon', 'transducer']
 
-for module in __all__:
-    importlib.import_module(f'.{module}', package='sequence.components')
 
 def __dir__():
     return sorted(__all__)
