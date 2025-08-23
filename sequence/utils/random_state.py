5fddb56ff32cffc07b47e7e049408378f3a3e713
Caitao Zhan <caitaozhan@gmail.com>
2025-08-22 22:42:54 -0500

    [major] fix bug in TeleportApp and TeleportProtocol -- a redesign to fix it

--- sequence/utils/random_state.py
+++ sequence/utils/random_state.py
@@ -2,11 +2,10 @@
 import math
 import random
 
-def random_state():
+def random_state() -> list:
+    """Generate a random state
+    """
     u = random.random()
     θ = 2 * math.acos(math.sqrt(u))
     φ = 2 * math.pi * random.random()
-    return [
-        math.cos(θ / 2),
-        complex(math.sin(θ / 2) * math.cos(φ), math.sin(θ / 2) * math.sin(φ))
-    ]
+    return [math.cos(θ / 2),complex(math.sin(θ / 2) * math.cos(φ), math.sin(θ / 2) * math.sin(φ))]
