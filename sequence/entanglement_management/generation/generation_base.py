88e8edbf66a642a34a4f599f075d17d4b10fe715
Caitao Zhan <caitaozhan@gmail.com>
2026-04-07 11:36:33 -0500

    [minor] update logging

--- sequence/entanglement_management/generation/generation_base.py
+++ sequence/entanglement_management/generation/generation_base.py
@@ -135,7 +135,7 @@ class EntanglementGenerationA(EntanglementProtocol, ABC):
             Will send message through attached node.
         """
 
-        log.logger.info(f"{self.name} protocol start with partner {self.remote_protocol_name}")
+        log.logger.info(f"{self.owner.name} {self.name} protocol start with partner {self.remote_protocol_name}")
 
         if self not in self.owner.protocols:
             return
