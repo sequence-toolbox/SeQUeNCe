6da191e46f40f8d252ae5aa2c1cbb07ab966064c
Robert Hayek <hayekr2@gmail.com>
2026-04-25 20:47:55 -0500

    Create custom topology command using GML

--- sequence/utils/config_generator_cli.py
+++ sequence/utils/config_generator_cli.py
@@ -5,6 +5,8 @@ This module provides multiple commands to generate different types of graphs bas
 The generated configurations can then be used in simulation frameworks with specific parameters,
 including quantum channel distance, link attenuation, classical channel delay, and measurement fidelity.
 
+It is possible to create a topology using a custom graph by passing a GML file.
+
 Classes:
     app: The main Typer application for CLI commands.
 
@@ -25,74 +27,90 @@ Example:
     generate-topology <topology> <parameters>; Generate <topology> with parameters <parameters>
     generate-topology --help; Display possible topologies.
     generate-topology <topology> --help; Display parameters for a topology type.
-
+    generate-topology custom <gml_path.gml>; Create a custom topology from a GML file.
 """
-import json
 
-import typer
+import json
+import yaml
 from typing import Annotated
-from .graphs import (build_caveman, build_grid, build_star, build_linear, build_mesh, build_ring, build_waxman,
-                     build_tree, build_autonomous_system, build_bcube, build_k_n)
+
+import networkx as nx
+import typer
+
+from .graphs import (
+    build_autonomous_system,
+    build_bcube,
+    build_caveman,
+    build_grid,
+    build_k_n,
+    build_linear,
+    build_mesh,
+    build_ring,
+    build_star,
+    build_tree,
+    build_waxman,
+)
 from .nx_converter import generate_config
 
 app = typer.Typer()
 
+CCDelay = Annotated[
+    float, typer.Option(help="Constant delay of the classical channel (ms)")
+]
+MemorySize = Annotated[int, typer.Option(help="Number of quantum memories per node")]
+OutputFile = Annotated[str, typer.Option(help="Name of the output file")]
+OutputDirectory = Annotated[str, typer.Option(help="Name of the output directory")]
+StopTime = Annotated[float, typer.Option(help="Stop time of the simulation (s)")]
+Formalism = Annotated[str, typer.Option(help="Formalism of the QuantumManager")]
+Template = Annotated[str, typer.Option(help="Path of the template JSON file")]
+GateFidelity = Annotated[float, typer.Option(help="Fidelity of the CNOT Gate")]
+MeasurementFidelity = Annotated[float, typer.Option(help="Fidelity of the Measurement")]
+Seed = Annotated[int | None, typer.Option(help="RNG seed for random graph generation")]
+GMLPath = Annotated[str, typer.Argument(help="Path of the .gml file.")]
 
-QcLength = Annotated[float, typer.Argument(help='Distance of the quantum channel (km)')]
-QcAttenuation = Annotated[float, typer.Option(help='Attenuation of the link (dB/m)')]
-CCDelay = Annotated[float, typer.Option(help='Constant delay of the classical channel (ms)')]
-OutputFile = Annotated[str, typer.Option(help='Name of the output file')]
-OutputDirectory = Annotated[str, typer.Option(help='Name of the output directory')]
-StopTime = Annotated[float, typer.Option(help='Stop time of the simulation (s)')]
-Formalism = Annotated[str, typer.Option(help='Formalism of the QuantumManager')]
-Template = Annotated[str, typer.Option(help='Path of the template JSON file')]
-GateFidelity = Annotated[float, typer.Option(help='Fidelity of the CNOT Gate')]
-MeasurementFidelity = Annotated[float, typer.Option(help='Fidelity of the Measurement')]
-Seed = Annotated[int|None, typer.Option(help='RNG seed for random graph generation')]
 
 def get_template(template_path: str) -> dict:
-    with open(template_path, 'r') as f:
+    with open(template_path, "r") as f:
         data = json.load(f)
     return data
+
 
 @app.command()
 def caveman(
     cliques: Annotated[int, typer.Argument(help="Number of cliques")],
     size: Annotated[int, typer.Argument(help="Size of cliques")],
-    qc_length: QcLength,
-    qc_attenuation: QcAttenuation = 0.0002,
     cc_delay: CCDelay = 1,
-    output: OutputFile = 'output.json',
-    directory: OutputDirectory = 'tmp',
+    memory_size: MemorySize = 1,
+    output: OutputFile = "output.json",
+    directory: OutputDirectory = "tmp",
     stop_time: StopTime = float("inf"),
-    formalism: Formalism = 'bell_diagonal',
-    template_path: Template = '',
+    formalism: Formalism = "bell_diagonal",
+    template_path: Template = "",
     measurement_fidelity: MeasurementFidelity = 1,
-    gate_fidelity: GateFidelity = 1
+    gate_fidelity: GateFidelity = 1,
 ) -> None:
     template = get_template(template_path) if template_path else None
     g = build_caveman(cliques, size)
     generate_config(
         g,
-        qc_length=qc_length,
-        qc_attn=qc_attenuation,
         cc_delay=cc_delay,
+        memory_size=memory_size,
         output_file=output,
         output_directory=directory,
         stop_time=stop_time,
         formalism=formalism,
         node_template=template,
         meas_fid=measurement_fidelity,
-        gate_fid=gate_fidelity
+        gate_fid=gate_fidelity,
     )
+
 
 @app.command()
 def grid(
     size_x: Annotated[int, typer.Argument(help="Number of nodes on the x-axis")],
     size_y: Annotated[int, typer.Argument(help="Number of nodes on the y-axis")],
-    qc_length: QcLength,
-    qc_attenuation: QcAttenuation = 0.0002,
     cc_delay: CCDelay = 1,
+    memory_size: MemorySize = 1,
     output: OutputFile = "output.json",
     directory: OutputDirectory = "tmp",
     stop_time: StopTime = float("inf"),
@@ -105,9 +123,8 @@ def grid(
     g = build_grid(size_x, size_y)
     generate_config(
         g,
-        qc_length=qc_length,
-        qc_attn=qc_attenuation,
         cc_delay=cc_delay,
+        memory_size=memory_size,
         output_file=output,
         output_directory=directory,
         stop_time=stop_time,
@@ -117,12 +134,14 @@ def grid(
         gate_fid=gate_fidelity,
     )
 
+
 @app.command()
 def star(
-    outer_nodes: Annotated[int, typer.Argument(help="Number of nodes connected to the center")],
-    qc_length: QcLength,
-    qc_attenuation: QcAttenuation = 0.0002,
+    outer_nodes: Annotated[
+        int, typer.Argument(help="Number of nodes connected to the center")
+    ],
     cc_delay: CCDelay = 1,
+    memory_size: MemorySize = 1,
     output: OutputFile = "output.json",
     directory: OutputDirectory = "tmp",
     stop_time: StopTime = float("inf"),
@@ -135,25 +154,23 @@ def star(
     g = build_star(outer_nodes)
     generate_config(
         g,
-        qc_length=qc_length,
-        qc_attn=qc_attenuation,
         cc_delay=cc_delay,
+        memory_size=memory_size,
         output_file=output,
         output_directory=directory,
         stop_time=stop_time,
         formalism=formalism,
         node_template=template,
         meas_fid=measurement_fidelity,
-        gate_fid=gate_fidelity
+        gate_fid=gate_fidelity,
     )
 
 
 @app.command()
 def linear(
     nodes: Annotated[int, typer.Argument(help="Number of nodes in the chain")],
-    qc_length: QcLength,
-    qc_attenuation: QcAttenuation = 0.0002,
     cc_delay: CCDelay = 1,
+    memory_size: MemorySize = 1,
     output: OutputFile = "output.json",
     directory: OutputDirectory = "tmp",
     stop_time: StopTime = float("inf"),
@@ -166,9 +183,8 @@ def linear(
     g = build_linear(nodes)
     generate_config(
         g,
-        qc_length=qc_length,
-        qc_attn=qc_attenuation,
         cc_delay=cc_delay,
+        memory_size=memory_size,
         output_file=output,
         output_directory=directory,
         stop_time=stop_time,
@@ -183,9 +199,8 @@ def linear(
 def mesh(
     size_x: Annotated[int, typer.Argument(help="Number of nodes on the x-axis")],
     size_y: Annotated[int, typer.Argument(help="Number of nodes on the y-axis")],
-    qc_length: QcLength,
-    qc_attenuation: QcAttenuation = 0.0002,
     cc_delay: CCDelay = 1,
+    memory_size: MemorySize = 1,
     output: OutputFile = "output.json",
     directory: OutputDirectory = "tmp",
     stop_time: StopTime = float("inf"),
@@ -198,9 +213,8 @@ def mesh(
     g = build_mesh(size_x, size_y)
     generate_config(
         g,
-        qc_length=qc_length,
-        qc_attn=qc_attenuation,
         cc_delay=cc_delay,
+        memory_size=memory_size,
         output_file=output,
         output_directory=directory,
         stop_time=stop_time,
@@ -214,9 +228,8 @@ def mesh(
 @app.command()
 def ring(
     nodes: Annotated[int, typer.Argument(help="Number of nodes in the ring")],
-    qc_length: QcLength,
-    qc_attenuation: QcAttenuation = 0.0002,
     cc_delay: CCDelay = 1,
+    memory_size: MemorySize = 1,
     output: OutputFile = "output.json",
     directory: OutputDirectory = "tmp",
     stop_time: StopTime = float("inf"),
@@ -229,9 +242,8 @@ def ring(
     g = build_ring(nodes)
     generate_config(
         g,
-        qc_length=qc_length,
-        qc_attn=qc_attenuation,
         cc_delay=cc_delay,
+        memory_size=memory_size,
         output_file=output,
         output_directory=directory,
         stop_time=stop_time,
@@ -245,9 +257,8 @@ def ring(
 @app.command()
 def waxman(
     nodes: Annotated[int, typer.Argument(help="Number of nodes in the Waxman graph")],
-    qc_length: QcLength,
-    qc_attenuation: QcAttenuation = 0.0002,
     cc_delay: CCDelay = 1,
+    memory_size: MemorySize = 1,
     output: OutputFile = "output.json",
     directory: OutputDirectory = "tmp",
     stop_time: StopTime = float("inf"),
@@ -261,9 +272,8 @@ def waxman(
     g = build_waxman(nodes, seed=seed)
     generate_config(
         g,
-        qc_length=qc_length,
-        qc_attn=qc_attenuation,
         cc_delay=cc_delay,
+        memory_size=memory_size,
         output_file=output,
         output_directory=directory,
         stop_time=stop_time,
@@ -280,9 +290,8 @@ def tree(
         int, typer.Argument(help="Branching factor of the tree")
     ],
     nodes: Annotated[int, typer.Argument(help="Number of nodes in the tree")],
-    qc_length: QcLength,
-    qc_attenuation: QcAttenuation = 0.0002,
     cc_delay: CCDelay = 1,
+    memory_size: MemorySize = 1,
     output: OutputFile = "output.json",
     directory: OutputDirectory = "tmp",
     stop_time: StopTime = float("inf"),
@@ -295,9 +304,8 @@ def tree(
     g = build_tree(branching_factor, nodes)
     generate_config(
         g,
-        qc_length=qc_length,
-        qc_attn=qc_attenuation,
         cc_delay=cc_delay,
+        memory_size=memory_size,
         output_file=output,
         output_directory=directory,
         stop_time=stop_time,
@@ -311,9 +319,8 @@ def tree(
 @app.command()
 def autonomous_system(
     nodes: Annotated[int, typer.Argument(help="Number of nodes in the AS graph")],
-    qc_length: QcLength,
-    qc_attenuation: QcAttenuation = 0.0002,
     cc_delay: CCDelay = 1,
+    memory_size: MemorySize = 1,
     output: OutputFile = "output.json",
     directory: OutputDirectory = "tmp",
     stop_time: StopTime = float("inf"),
@@ -327,9 +334,8 @@ def autonomous_system(
     g = build_autonomous_system(nodes, seed=seed)
     generate_config(
         g,
-        qc_length=qc_length,
-        qc_attn=qc_attenuation,
         cc_delay=cc_delay,
+        memory_size=memory_size,
         output_file=output,
         output_directory=directory,
         stop_time=stop_time,
@@ -344,9 +350,8 @@ def autonomous_system(
 def bcube(
     k: Annotated[int, typer.Argument(help="Number of BCube levels (k >= 1)")],
     n: Annotated[int, typer.Argument(help="Number of ports per switch")],
-    qc_length: QcLength,
-    qc_attenuation: QcAttenuation = 0.0002,
     cc_delay: CCDelay = 1,
+    memory_size: MemorySize = 1,
     output: OutputFile = "output.json",
     directory: OutputDirectory = "tmp",
     stop_time: StopTime = float("inf"),
@@ -359,9 +364,8 @@ def bcube(
     g = build_bcube(k, n)
     generate_config(
         g,
-        qc_length=qc_length,
-        qc_attn=qc_attenuation,
         cc_delay=cc_delay,
+        memory_size=memory_size,
         output_file=output,
         output_directory=directory,
         stop_time=stop_time,
@@ -376,9 +380,8 @@ def bcube(
 def k_n(
     k: Annotated[int, typer.Argument(help="Number of ports per switch")],
     n: Annotated[int, typer.Argument(help="Number of levels in the fat tree")],
-    qc_length: QcLength,
-    qc_attenuation: QcAttenuation = 0.0002,
     cc_delay: CCDelay = 1,
+    memory_size: MemorySize = 1,
     output: OutputFile = "output.json",
     directory: OutputDirectory = "tmp",
     stop_time: StopTime = float("inf"),
@@ -391,9 +394,37 @@ def k_n(
     g = build_k_n(k, n)
     generate_config(
         g,
-        qc_length=qc_length,
-        qc_attn=qc_attenuation,
         cc_delay=cc_delay,
+        memory_size=memory_size,
+        output_file=output,
+        output_directory=directory,
+        stop_time=stop_time,
+        formalism=formalism,
+        node_template=template,
+        meas_fid=measurement_fidelity,
+        gate_fid=gate_fidelity,
+    )
+
+
+@app.command()
+def custom(
+    gml_path: GMLPath,
+    cc_delay: CCDelay = 1,
+    memory_size: MemorySize = 1,
+    output: OutputFile = "output.json",
+    directory: OutputDirectory = "tmp",
+    stop_time: StopTime = float("inf"),
+    formalism: Formalism = "bell_diagonal",
+    template_path: Template = "",
+    measurement_fidelity: MeasurementFidelity = 1,
+    gate_fidelity: GateFidelity = 1,
+) -> None:
+    template = get_template(template_path) if template_path else None
+    g: nx.Graph = nx.read_gml(gml_path)
+    generate_config(
+        g,
+        cc_delay=cc_delay,
+        memory_size=memory_size,
         output_file=output,
         output_directory=directory,
         stop_time=stop_time,
