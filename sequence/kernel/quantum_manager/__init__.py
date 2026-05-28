599b8626114fe64e8a6d5c4681d0caa644280d50
Caitao Zhan <caitaozhan@gmail.com>
2026-05-28 17:14:14 -0500

    [minor] rename files

--- sequence/kernel/quantum_manager/__init__.py
+++ sequence/kernel/quantum_manager/__init__.py
@@ -1,7 +1,7 @@
 from .base import QuantumManager
-from .ket import QuantumManagerKet
-from .density import QuantumManagerDensity
-from .fock import QuantumManagerDensityFock
+from .ket_vector import QuantumManagerKet
+from .density_matrix import QuantumManagerDensity
+from .fock_density_matrix import QuantumManagerDensityFock
 from .bell_diagonal import QuantumManagerBellDiagonal
 
 __all__ = [
